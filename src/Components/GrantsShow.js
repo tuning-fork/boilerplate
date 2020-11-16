78456f8d5ed454686820a2a30c0721ee8e4a05e2
mike <mikemcfaddin@gmail.com>
2020-11-16 12:16:15 -0600

    updated grants index with add grant at top, removed create report on grants, sstarting to update grantsshow with new user flow

--- src/Components/GrantsShow.js
+++ src/Components/GrantsShow.js
@@ -142,160 +142,168 @@ class GrantsShow extends Component {
     }
     return (
       <div className="component">
-      <Card>
-        <Card.Header>
-          <h2>Title: {this.state.title}</h2>
-        </Card.Header>
-        <Card.Body>
-          <h3>RFP URL: {this.state.rfp_url}</h3>
-          <h3>Deadline: {this.state.deadline}</h3>
-          <h3>Submitted: {this.state.submitted}</h3>
-          <h3>Successful: {this.state.successful}</h3>
-          <h3>Purpose: {this.state.purpose}</h3>
-        </Card.Body>
-        <Card.Header>
-          <h2>Sections:</h2>
-        </Card.Header>
-        <Card.Body>
-        {this.state.sections.map(section => {
-            return(
-              <div key={section.id}>
-                <h4>{section.title}</h4>
-                <h4>{section.text}</h4>
-                <h4>Wordcount: {section.wordcount}</h4>
-                <h4>Sort Order: {section.sort_order}</h4>
-                <br/>
+        <Card>
+          <Card.Header>
+            <h2>Title: {this.state.title}</h2>
+          </Card.Header>
+          <Card.Body>
+            <h3>RFP URL: {this.state.rfp_url}</h3>
+            <h3>Deadline: {this.state.deadline}</h3>
+            <h3>Submitted: {this.state.submitted ? "yes" : "not yet"}</h3>
+            <h3>Successful: {this.state.successful ? "yes" : "not yet"}</h3>
+            <h3>Purpose: {this.state.purpose}</h3>
+
+            {/* beginning of grant update */}
+
+            <Button onClick={this.toggleHidden.bind(this)}>
+              Update Grant
+            </Button>
+            <br />
+            {this.state.isHidden ? (
+              <div>
+                <div>
+                  <Form onSubmit={this.handleSubmit}>
+                    <Form.Group>
+                      <Form.Label>Title</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={this.state.title}
+                        name="title"
+                        // placeholder={this.state.title}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>RFP URL</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={this.state.rfp_url}
+                        name="rfp_url"
+                        // placeholder={this.state.rfp_url}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Deadline</Form.Label>
+                      <Form.Control
+                        type="datetime"
+                        value={this.state.deadline}
+                        name="deadline"
+                        // placeholder={this.state.deadline}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Submitted</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={this.state.submitted}
+                        name="submitted"
+                        // placeholder={this.state.submitted}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Successful</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={this.state.successful}
+                        name="successful"
+                        // placeholder={this.state.successful}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Purpose</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={this.state.purpose}
+                        name="purpose"
+                        // placeholder={this.state.purpose}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <div className="text-center">
+                      <Button type="submit" >
+                        Submit
+                      </Button>
+                      <Button
+                        onClick={this.toggleHidden.bind(this)}
+                      >
+                        Close
+                      </Button>
+                    </div>
+                  </Form>
+                </div>
               </div>
-              )
-        })}
-        </Card.Body>
-        <Card.Header>
-          <h2>Reports:</h2>
-        </Card.Header>
-        <Card.Body>
-        {this.state.reports.map(report =>
-          {
-            return(
-              <div key={report.id}>
-                <h4>{report.title}</h4>
-                <h4>{report.deadline}</h4>
-                <h4>{report.submitted}</h4>
-              </div>
-              )
-          })}
-        </Card.Body>
-      </Card>
+            ) : null}
+          </Card.Body>
+
+          {/* end of grant update, beginning of sections and reports */}
+
+          <Card.Header>
+            <h2>Sections:</h2>
+          </Card.Header>
+          <Card.Body>
+            {this.state.sections.length ? this.state.sections.map(section => {
+                return(
+                  <div key={section.id}>
+                    <h4>{section.title}</h4>
+                    <h4>{section.text}</h4>
+                    <h4>Wordcount: {section.wordcount}</h4>
+                    <h4>Sort Order: {section.sort_order}</h4>
+                    <br/>
+                  </div>
+                )
+              }) : <h3>This grant has no sections yet.</h3>
+            }
+            <h3>Add A Section:</h3>
+            <SectionsNew 
+              sort_number={this.state.sections.length}
+              grant_id={this.state.id} 
+              updateSections={this.updateSections}
+            />
+          </Card.Body>
+        
+          {/* reports */}
+
+          <Card.Header>
+            <h2>Reports:</h2>
+          </Card.Header>
+          <Card.Body>
+            {this.state.reports.length ? this.state.reports.map(report =>
+              {
+                return(
+                  <div key={report.id}>
+                    <h4>{report.title}</h4>
+                    <h4>{report.deadline}</h4>
+                    <h4>{report.submitted}</h4>
+                  </div>
+                  )
+              }) : <h3>This grant has no reports yet.</h3>
+            }
+          </Card.Body>
+        </Card>
         <br />
 
-        {/* beginning of grant update if current user created grant */}
+        {/* beginning of grant update */}
 
-        <div>
-            <div className="container">
-              <Button onClick={this.toggleHidden.bind(this)}>
-                Update Grant
-              </Button>
-              <br />
-              <br />
-              {this.state.isHidden ? (
-                <div>
-                  <div>
-                    <Form onSubmit={this.handleSubmit}>
-                      <Form.Group>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={this.state.title}
-                          name="title"
-                          // placeholder={this.state.title}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>RFP URL</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={this.state.rfp_url}
-                          name="rfp_url"
-                          // placeholder={this.state.rfp_url}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Deadline</Form.Label>
-                        <Form.Control
-                          type="datetime"
-                          value={this.state.deadline}
-                          name="deadline"
-                          // placeholder={this.state.deadline}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Submitted</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={this.state.submitted}
-                          name="submitted"
-                          // placeholder={this.state.submitted}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Successful</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={this.state.successful}
-                          name="successful"
-                          // placeholder={this.state.successful}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Purpose</Form.Label>
-                        <Form.Control
-                          type="text"
-                          value={this.state.purpose}
-                          name="purpose"
-                          // placeholder={this.state.purpose}
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </Form.Group>
-                      <div className="text-center">
-                        <Button type="submit" >
-                          Submit
-                        </Button>
-                        <Button
-                          onClick={this.toggleHidden.bind(this)}
-                        >
-                          Close
-                        </Button>
-                      </div>
-                    </Form>
-                  </div>
-                </div>
-              ) : null}
-            </div>
-        </div>
+        
+     
 
         {/* beginning of SectionsNew */}
 
         <br />
-        <h3>Add A Section:</h3>
-        <SectionsNew 
-          sort_number={this.state.sections.length}
-          grant_id={this.state.id} 
-          history={this.props.history}
-          updateSections={this.updateSections}
-        />
+        
         <br />
 
-        <Button onClick={this.handleGrantDelete}>Delete</Button>
+        <Button onClick={this.handleGrantDelete}>Delete Grant</Button>
         <Link 
           to={`/grants-finalize/${this.state.id}`}
         >
