2208e7f06fd1c857d203d28bae13a4d366778865
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-12-17 10:50:58 -0600

    continued working on filtering functions

--- src/Components/Boilerplates.js
+++ src/Components/Boilerplates.js
@@ -21,6 +21,7 @@ class Boilerplates extends Component {
       query: '',
       searchText: '',
       filterWordCount: false,
+      filteredByWordCount: [],
       filterTitle: false,
       filterText: false
     };
@@ -96,15 +97,77 @@ class Boilerplates extends Component {
     });
   };
 
+  // <button type="button" onclick="alert(output)">Click Me!</button>
+  // <script>
+  // var input = new Array(9,3,4.3,24,54,8,19,23,46,87,3.14);
+  // var output = new Array();
+  // for (var i = 0; i < input.length; i ++) {
+  // if(input[i] > 10)
+  // {
+  // output.push(input[i]);
+  // }
+  // }
+
+//   var input = new Array(9,3,4.3,24,54,8,19,23,46,87,3.14);
+    //var output = new Array();
+
+    // input = input.sort(function(a, b) {
+    //   return a - b
+    // }).filter(function(val, key) {
+    //   return val < 10 ? val : output.push(val) && null
+    // })
+
+// console.log(input, output);
+
+
   handleChange = (event) => {
+    const searchValue = event.target.value.toLowerCase()
+    if (this.state.filterWordCount === true) {
+      console.log(searchValue, "wordcount filter")
+      let filteredByWordCount = [];
+      filteredByWordCount = this.state.boilerplates.filter((boilerplate) => {
+        return boilerplate.wordcount < searchValue ? boilerplate : null
+      })
+      this.setState({filteredByWordCount: this.state.filteredByWordCount})
+      console.log(this.filteredByWordCount)
+    }
+    else if (this.state.filterTitle) {
+      console.log(searchValue, "title filter")
+      console.log(this.state.boilerplates)
+      let filteredByTitle = [];
+      filteredByTitle = this.state.boilerplates.filter((boilerplate) => {
+        return boilerplate.title.toLowerCase().indexOf(searchValue) !== -1
+      })
+      console.log(filteredByTitle)
+    }
+
+    else if (this.state.filterText) {
+      console.log(searchValue, "text filter")
+      let filteredByText = [];
+    }
     this.setState({
       searchText: event.target.value
     })
   }
 
-  handleSubmit = (event) => {
+  // handleSubmit = (event) => {
+  //   const value = this.state.searchText
+  //   if (this.state.filterWordCount === true) {
+  //     console.log(value)
+  //     // let filteredByWordCount = [];
+  //     // filteredByWordCount = this.state.boilerplates.filter((boilerplate) => {
+  //     //   return boilerplate.wordcount <= value.parseInt()
+  //     // })
+  //     // this.setState({filteredByWordCount: this.state.filteredByWordCount})
+  //   }
+  //   else if (this.state.filterTitle) {
 
-  }
+  //   }
+
+  //   else if (this.state.filterText) {
+
+  //   }
+  // }
 
   //Filter by: 
 
@@ -130,9 +193,11 @@ class Boilerplates extends Component {
   
 
   render() {
-    console.log(this.state.filterWordCount, "waffle")
-    console.log(this.state.filterTitle, "pancake")
-    console.log(this.state.filterText, "crepe")
+    // console.log(this.state.filterWordCount, "waffle")
+    // console.log(this.state.filterTitle, "pancake")
+    // console.log(this.state.filterText, "crepe")
+
+    console.log(this.state.filteredByWordCount)
     if (this.state.loading) {
       return <h1>Loading....</h1>;
     };
@@ -168,11 +233,29 @@ class Boilerplates extends Component {
         />
         <br/>
 
-        <button onClick={(event) => this.setState({filterWordCount: !this.state.filterWordCount})}>Filter by WordCount</button>
-        <button onClick={(event) => this.setState({filterTitle: !this.state.filterTitle})}>Filter by Title</button>
-        <button onClick={(event) => this.setState({filterText: !this.state.filterText})}>Filter by Text</button>
+        <button onClick={(event) => 
+          this.setState({
+            filterWordCount: true,
+            filterTitle: false,
+            filterText: false
+          })}
+            >Filter by WordCount</button>
+        <button onClick={(event) => 
+          this.setState({
+            filterTitle: true,
+            filterWordCount: false,
+            filterText: false
+          })}
+            >Filter by Title</button>
+        <button onClick={(event) => 
+          this.setState({
+            filterText: true,
+            filterTitle: false,
+            filterWordCount: false
+          })}
+            >Filter by Text</button>
         
-        <Form onSubmit={this.handleSubmit}>
+        <Form>
           <Form.Group>
           <Form.Label></Form.Label>
           <Form.Control 
@@ -180,9 +263,6 @@ class Boilerplates extends Component {
             value={this.state.searchText} 
             onChange={this.handleChange} />
           </Form.Group>
-          <Button
-          type="submit"
-          >Search</Button>
         </Form>
 
         {this.state.boilerplates.map((boilerplate) => {
