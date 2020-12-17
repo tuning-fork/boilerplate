b6a772fabb5047fdc33aa8e47e6f4d9e8423b5a9
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-12-16 22:26:48 -0600

    added text to explainer section?

--- src/Components/Explainer.js
+++ src/Components/Explainer.js
@@ -5,6 +5,13 @@ function Explainer() {
     <div>
       <div className="component">
         <h1>How to Write a Grant</h1>
+        <h1>Grant Research</h1>
+        <h1>Grant Outreach (?)</h1>
+        <h1>Letters of Intent</h1>
+        <h1>Grant Forms</h1>
+        <h1>Grant Draft (need sections)</h1>
+        <h1>???</h1>
+        
       </div>
     </div>
   );
