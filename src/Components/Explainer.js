42eec38dbadd12b169c29f70520aa32f2dfdc3ff
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-12-16 22:20:13 -0600

    added explainer file

---
+++ src/Components/Explainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Explainer() {
+  return (
+    <div>
+      <div className="component">
+        <h1>How to Write a Grant</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Explainer;
