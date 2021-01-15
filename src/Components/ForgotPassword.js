478e58f36d8a0d58b5cace772a6d91e9bba667b9
mike <mikemcfaddin@gmail.com>
2021-01-15 11:26:09 -0500

    removed unused code from ForgotPassword

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import { forgotPassword } from '../helpers/passwords';
+import axios from 'axios';
+// import { forgotPassword } from '../helpers/passwords';
 
 export default class ForgotPassword extends Component {
   state = {
     email: ""
   }
 
-    const baseURL = "http://localhost:3000/api/v1"
-
-    forgotPassword = (email) => {
-        axios
-            .post('/api/forgot_password', 
-            {headers: { Authorization: `Bearer ${localStorage.token}` }})
-            .then((response) => {
-                alert(response.alert)
-            })
-            .catch((error) => console.log(error));
-    }
+  forgotPassword = (email) => {
+    axios
+      .post('/api/forgot_password', 
+        {headers: { Authorization: `Bearer ${localStorage.token}` }})
+      .then((response) => {
+          alert(response.alert)
+      })
+      .catch((error) => console.log(error));
+  }
 
   handleChange = (event) => {
     const { name, value } = event.target
@@ -27,7 +26,7 @@ export default class ForgotPassword extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
-    forgotPassword(this.state.email)
+    this.forgotPassword(this.state.email)
     this.setState({
       email: ""
     })
