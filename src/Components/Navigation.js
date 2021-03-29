562b0c6e6c1abae02b8f96a8e034f7567884ab63
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-03-29 11:45:08 -0500

    finished refactoring for hooks

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,31 +1,35 @@
-import React, { Component, useState } from 'react';
-// import { Link, withRouter } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import React, { Component, useState } from "react";
+import { withRouter } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 function Navigation(props) {
-  const handleLogoutClick() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user_id');
-    props.history.push('/login');
-  }
+  const handleLogoutClick = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user_id");
+    props.history.push("/login");
+  };
 
-    return (
-        <Navbar bg="black" variant="dark" expand="lg" sticky="top" style={{paddingTop: "1rem", align: "right", backgroundColor: "#0e272a"}}>
-        
-          <ul>
-            {localStorage.token && localStorage.user_id ? (
-              <div>
-              <Nav className="mr-auto">
-                  <Nav.Item className="active">
-                  <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                  </Nav.Item>
-                <Nav.Item className="active">
-                  <Nav.Link href="/grants">Grants</Nav.Link>
-                </Nav.Item>
-                {/* <Nav.Item className="active">
+  return (
+    <Navbar
+      bg="black"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      style={{ paddingTop: "1rem", align: "right", backgroundColor: "#0e272a" }}
+    >
+      <ul>
+        {localStorage.token && localStorage.user_id ? (
+          <div>
+            <Nav className="mr-auto">
+              <Nav.Item className="active">
+                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="active">
+                <Nav.Link href="/grants">Grants</Nav.Link>
+              </Nav.Item>
+              {/* <Nav.Item className="active">
                   <Nav.Link href="/bios">Bios</Nav.Link>
                 </Nav.Item>
                 <Nav.Item className="active">
@@ -40,14 +44,16 @@ function Navigation(props) {
                 <Nav.Item className="active">
                   <Nav.Link href="/funding_orgs">Funding Orgs</Nav.Link>
                 </Nav.Item> */}
-                <Nav.Item>
-                  <Nav.Link onClick={handleLogoutClick}to="/logout">Logout</Nav.Link>
-                </Nav.Item>
-                </Nav>
-                </div>
-                ) : (
-                <div>
-                {/* <Nav>
+              <Nav.Item>
+                <Nav.Link onClick={handleLogoutClick} to="/logout">
+                  Logout
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </div>
+        ) : (
+          <div>
+            {/* <Nav>
                 <Nav.Item className="active">
                   <Nav.Link href="/login">Login</Nav.Link>
                 </Nav.Item>
@@ -55,17 +61,35 @@ function Navigation(props) {
                   <Nav.Link href="/signup">Sign Up</Nav.Link>
                 </Nav.Item>
                 </Nav> */}
-                <Button href="/signup" variant="outline-success" style={{textColor: "#23cb87", fontWeight: "bolder", display: "inline", margin: "1rem"}}>
-                Sign Up
-                </Button>
-                <Button href="/login" variant="outline-success" style={{textColor: "#23cb87", fontWeight: "bolder", display: "inline", margin: "1rem"}}>
-                Log In
-                </Button>
-              </div>
-            )}
-          </ul>
-        </Navbar>
-    );
-  }
+            <Button
+              href="/signup"
+              variant="outline-success"
+              style={{
+                textColor: "#23cb87",
+                fontWeight: "bolder",
+                display: "inline",
+                margin: "1rem",
+              }}
+            >
+              Sign Up
+            </Button>
+            <Button
+              href="/login"
+              variant="outline-success"
+              style={{
+                textColor: "#23cb87",
+                fontWeight: "bolder",
+                display: "inline",
+                margin: "1rem",
+              }}
+            >
+              Log In
+            </Button>
+          </div>
+        )}
+      </ul>
+    </Navbar>
+  );
+}
 
 export default withRouter(Navigation);
