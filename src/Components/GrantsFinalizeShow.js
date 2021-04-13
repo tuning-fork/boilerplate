bb554a9aa79d665cdcd87e289f9399fa604e1b2c
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-04-13 16:04:54 -0500

    changed some routes in finalize

--- src/Components/GrantsFinalizeShow.js
+++ src/Components/GrantsFinalizeShow.js
@@ -124,7 +124,7 @@ export default function GrantsFinalizeShow(props) {
     event.preventDefault();
     axios
       .patch(
-        "/api/organizations/${currentOrganizationStore.currentOrganizationInfo.id}/grants/" +
+        `/api/organizations/${currentOrganizationStore.currentOrganizationInfo.id}/grants/` +
           id,
         {
           title: title,
@@ -376,6 +376,7 @@ export default function GrantsFinalizeShow(props) {
                 isUnzipped={section.isUnzipped}
                 toggleUnzipped={toggleUnzipped}
                 section_id={section.id}
+                grant_id={id}
                 boilerplates={boilerplates}
                 bios={bios}
                 // section_title={section.title}
