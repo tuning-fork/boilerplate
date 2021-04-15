37752e2cd48dfed85eb11ec411ca81fb6b316c3a
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-04-14 19:55:49 -0500

    started building splash page

---
+++ src/Components/OrgContextSelect.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useCurrentUserContext } from "../Contexts/currentUserContext";
+import { useCurrentOrganizationContext } from "../Contexts/currentOrganizationContext";
+
+export default function OrgContextSelect() {
+  const [currentUserStore, currentUserDispatch] = useCurrentUserContext();
+  const [
+    currentOrganizationStore,
+    currentOrganizationDispatch,
+  ] = useCurrentOrganizationContext();
+  const [organizationId, setOrganizationId] = useState("");
+  const [organizations, setOrganizations] = useState([]);
+
+  const handleLogoutClick = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user_id");
+    props.history.push("/landing_page");
+  };
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    const selectedOrgInfo = currentOrganizationStore.allUserOrganizations.filter(
+      (userOrganization) => event.target.value == userOrganization.id
+    );
+    currentOrganizationDispatch({
+      type: "SET_CURRENT_ORGANIZATION_INFO",
+      payload: selectedOrgInfo[0],
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex-row row" style={{ paddingBottom: ".5rem" }}>
+        <div className="w-100">
+          <h1>Welcome, {currentUserStore?.currentUserInfo?.first_name}</h1>
+        </div>
+      </div>
+      <Form.Group>
+        <Form.Label>Organization</Form.Label>
+        <Form.Control
+          as="select"
+          name="organizationId"
+          value={
+            currentOrganizationStore.currentOrganizationInfo == null
+              ? "0"
+              : currentOrganizationStore.currentOrganizationInfo.id
+          }
+          onChange={handleChange}
+          required
+        >
+          <option value="0" disabled>
+            Select Organization
+          </option>
+          {currentOrganizationStore.allUserOrganizations?.map(
+            (userOrganization) => {
+              return (
+                <option
+                  key={userOrganization.id}
+                  value={userOrganization.id}
+                  // onChange={(event) => {
+                  //   currentOrganizationDispatch({
+                  //     type: "SET_CURRENT_ORGANIZATION_INFO",
+                  //     payload: userOrganization,
+                  //   });
+                  // }}
+                >
+                  {userOrganization.name}
+                </option>
+              );
+            }
+          )}
+        </Form.Control>
+      </Form.Group>
+    </div>
+  );
+}
