3fdbe930760688b68151f275c4e0abbdd2984987
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-04-22 19:07:26 -0500

    added check to grants index - need to implement throughout

--- src/Components/Grants.js
+++ src/Components/Grants.js
@@ -20,6 +20,9 @@ export default function Grants() {
     currentOrganizationStore,
     currentOrganizationDispatch,
   ] = useCurrentOrganizationContext();
+  const currentOrganizationId =
+    currentOrganizationStore.currentOrganizationInfo &&
+    currentOrganizationStore.currentOrganizationInfo.id;
 
   const [show, setShow] = useState(false);
 
@@ -44,24 +47,25 @@ export default function Grants() {
     setFilteredGrants(alteredGrants);
   };
 
+  console.log("grant render");
+
   useEffect(() => {
     console.log("use effect ran");
-    axios
-      .get(
-        `/api/organizations/${currentOrganizationStore.currentOrganizationInfo.id}/grants`,
-        {
+    if (currentOrganizationId) {
+      axios
+        .get(`/api/organizations/${currentOrganizationId}/grants`, {
           headers: { Authorization: `Bearer ${localStorage.token}` },
-        }
-      )
-      .then((response) => {
-        const zippyGrants = createUnzipped(response.data);
-        console.log(zippyGrants);
-        setGrants(response.data);
-        setFilteredGrants(zippyGrants);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, [currentOrganizationStore.currentOrganizationInfo.id]);
+        })
+        .then((response) => {
+          const zippyGrants = createUnzipped(response.data);
+          console.log(zippyGrants);
+          setGrants(response.data);
+          setFilteredGrants(zippyGrants);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [currentOrganizationId]);
 
   const updateGrants = (newGrant) => {
     const newGrants = [...grants];
@@ -404,34 +408,6 @@ export default function Grants() {
 
         {highlightedGrants}
       </div>
-      {/* ) : null} */}
-      {/* {this.state.grants.map((grant) => {
-          return (
-            <div key={grant.id}>
-              <Card>
-                <Card.Header> 
-                  <Link
-                    to={`/grants/${grant.id}`}
-                  >
-                    {grant.title}
-                  </Link>
-                </Card.Header>
-                <Card.Body>
-                  <p>Purpose: {grant.purpose}</p>
-                  <p>Funding Organization: {grant.funding_org_name}</p>
-                  <p>RFP URL: {grant.rfp_url}</p>
-                  <p>Deadline: {this.formatDate(grant.deadline)}</p>
-                  <p>Deadline: <Moment>{grant.deadline}</Moment></p>
-                  <Moment fromNow>{grant.deadline}</Moment>
-                  <p>Submitted: {grant.submitted ? "yes" : "not yet"}</p>
-                  <p>Successful: {grant.successful ? "yes" : "not yet"}</p>
-                  <p>Organization Name: {grant.organization_name}</p>
-                </Card.Body>
-              </Card>
-              <br />
-            </div>
-          );
-        })} */}
     </div>
   );
 }
