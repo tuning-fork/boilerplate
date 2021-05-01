8bf3dac68c45fd0582a36f3ff64ce247c4866a67
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-04-30 20:28:29 -0500

    refactored BoilerplatesShow component

---
+++ src/Components/Boilerplates/BoilerplateShowForm.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { useCurrentOrganizationContext } from "../../Contexts/currentOrganizationContext";
+import countWords from "../../Helpers/countWords";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["clean"],
+    [{ color: [] }],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "color",
+];
+
+export default function BoilerplatesShowForm(props) {
+  const { categories, onSubmit, onCancel } = props;
+  const [newTitle, setNewTitle] = useState(props.title);
+  const [newQuillText, setNewQuillText] = useState(props.quillText);
+  const [newCategoryId, setNewCategoryId] = useState(props.categoryId);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({
+      newTitle,
+      newQuillText,
+      newCategoryId,
+    });
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    onCancel();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group style={{ display: "l" }}>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          value={newTitle}
+          name="newTitle"
+          onChange={(event) => setNewTitle(event.target.value)}
+          required
+        />
+      </Form.Group>
+      <ReactQuill
+        name="newQuillText"
+        modules={modules}
+        format={formats}
+        defaultValue={props.quillText}
+        onChange={(value) => setNewQuillText(value)}
+        style={{ backgroundColor: "#fefefe", color: "black" }}
+      />
+      <p style={{ color: "#fefefe" }}>Word Count: {countWords(newQuillText)}</p>
+      <Form.Group>
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          as="select"
+          name="newCategoryId"
+          value={newCategoryId}
+          onChange={(event) => setNewCategoryId(event.target.value)}
+          required
+        >
+          <option value="" disabled>
+            Select Category
+          </option>
+          {categories.map((category) => {
+            return (
+              <option
+                key={category.id}
+                value={category.id}
+                onChange={(event) => setNewCategoryId(event.target.value)}
+              >
+                {category.name}
+              </option>
+            );
+          })}
+        </Form.Control>
+      </Form.Group>
+      <div>
+        <Button
+          variant="outline-success"
+          type="submit"
+          style={{
+            maxWidth: "50%",
+            align: "center",
+            backgroundColor: "#23cb87",
+            color: "#09191b",
+            fontWeight: "bolder",
+          }}
+          onClick={handleSubmit}
+        >
+          Save Changes
+        </Button>
+        <Button
+          variant="outline-success"
+          style={{
+            maxWidth: "50%",
+            align: "center",
+            backgroundColor: "#23cb87",
+            color: "#09191b",
+            fontWeight: "bolder",
+          }}
+          onClick={handleCancel}
+        >
+          Cancel
+        </Button>
+      </div>
+    </Form>
+  );
+}
--- src/Components/GrantsShow.js
+++ src/Components/GrantsShow.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import SectionsNew from "./SectionsNew";
