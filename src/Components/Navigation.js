236aa0a74209741c2285d2ceb3e996eb2f449874
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-05-01 22:14:06 -0500

    revised Navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,11 +4,12 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+// import { useHistory } from "react-router-dom";
 import { useCurrentUserContext } from "../Contexts/currentUserContext";
 import { useCurrentOrganizationContext } from "../Contexts/currentOrganizationContext";
 
 function Navigation(props) {
-  const [currentUserStore, currentUserDispatch] = useCurrentUserContext();
+  const [currentUserStore] = useCurrentUserContext();
   const [
     currentOrganizationStore,
     currentOrganizationDispatch,
