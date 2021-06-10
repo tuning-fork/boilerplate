111f46269951ac2db0c9ede44c44c9ca87053898
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-06-09 23:01:22 -0500

    Add more test cases for countWords

--- src/Helpers/countWords.test.js
+++ src/Helpers/countWords.test.js
@@ -6,6 +6,14 @@ describe("countWords", () => {
     expect(countWords("Lorem Ipsum.")).toEqual(2);
   });
 
+  it("counts digits as words", () => {
+    expect(countWords("The house is 120 years old.")).toEqual(6);
+  });
+
+  it("does not include whitespace, tabs, or newlines in word count", () => {
+    expect(countWords("\n\tThat is       great!\n")).toEqual(3);
+  });
+
   it("returns zero given empty string", () => {
     expect(countWords("")).toEqual(0);
   });
