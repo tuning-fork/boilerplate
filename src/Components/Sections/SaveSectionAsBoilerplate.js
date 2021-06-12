962caec23ddd87794eab6dd0e9e7243d77fc7a35
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-06-11 22:53:56 -0500

    added save as boilerplate modal to grant finalize show

--- src/Components/Sections/SaveSectionAsBoilerplate.js
+++ src/Components/Sections/SaveSectionAsBoilerplate.js
@@ -1,155 +1,175 @@
-// import React, { useRef, useState } from "react";
-// import { Link } from "react-router-dom";
-// import { Form, Button } from "react-bootstrap";
-// import ReactQuill from "react-quill";
-// import { countWords } from "../Services/infofunctions";
+import React, { useEffect, useRef, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import ReactQuill from "react-quill";
+import { useCurrentOrganizationContext } from "../../Contexts/currentOrganizationContext";
+import useBuildOrganizationsLink from "../../Hooks/useBuildOrganizationsLink";
+import { countWords } from "../../Services/infofunctions";
+import { createBoilerplate } from "../../Services/Organizations/BoilerplatesService";
+import { getAllCategories } from "../../Services/Organizations/CategoriesService";
 
-// export default function SaveSectionAsBoilerplate(props) {
-//   const quillEl = useRef(null);
-//   const [title, setTitle] = useState(props.section.title);
-//   const [text, setText] = useState(props.section.text);
-//   const [organizationId, setOrganizationId] = useState("");
-//   const [categoryId, setCategoryId] = useState("");
-//   const [wordcount, setWordcount] = useState("");
-//   //   const [categories, setCategories] = useState([]);
-//   //   const [
-//   //     isHiddenCategoriesOrganizationsNew,
-//   //     setIsHiddenCategoriesOrganizationsNew,
-//   //   ] = useState(true);
-//   const [errors, setErrors] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   //   const {
-//   //     currentOrganizationStore,
-//   //     currentOrganizationDispatch,
-//   //     organizationClient,
-//   //   } = useCurrentOrganizationContext();
-//   //   const currentOrganizationId =
-//   //     currentOrganizationStore.currentOrganization &&
-//   //     currentOrganizationStore.currentOrganization.id;
+export default function SaveSectionAsBoilerplate(props) {
+  const quillEl = useRef(null);
+  const [categories, setCategories] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { currentOrganizationStore } = useCurrentOrganizationContext();
+  const currentOrganizationId =
+    currentOrganizationStore.currentOrganization &&
+    currentOrganizationStore.currentOrganization.id;
 
-//   const handleCancel = (event) => {
-//     event.preventDefault();
-//     props.onCancel();
-//   };
+  const [newBoilerplateFields, setNewBoilerplateFields] = useState({
+    title: props.section.title,
+    text: props.section.text,
+    category_id: "",
+    wordcount: "",
+  });
+  const { organizationClient } = useCurrentOrganizationContext();
+  const buildOrganizationsLink = useBuildOrganizationsLink();
+  const { grant_id: grantId } = useParams();
+  const history = useHistory();
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const newBoilerplate = {
-//       title: title,
-//       text: text,
-//       organization_id: props.preventDefaultorganizationId,
-//       category_id: categoryId,
-//       wordcount: countWords(text),
-//     };
-//     createBoilerplate(organizationClient, newBoilerplate)
-//       .then((boilerplate) => {
-//         if (boilerplate) {
-//           // handle close save as boilerplate modal
-//         }
-//       })
-//       .catch((error) => {
-//         console.log("boilerplate creation error", error);
-//       });
-//   };
+  const handleChangeField = (field) => (event) => {
+    event.preventDefault();
 
-//   //   event.preventDefault();
-//   //     props.onSubmit({
-//   //       ...boilerplateFields,
-//   //       text: quillEl.current.getEditor().getText(),
-//   //     });
+    const newValue = event.target.value;
 
-//   const modules = {
-//     toolbar: [
-//       [{ header: [1, 2, false] }],
-//       ["bold", "italic", "underline", "strike", "blockquote"],
-//       [
-//         { list: "ordered" },
-//         { list: "bullet" },
-//         { indent: "-1" },
-//         { indent: "+1" },
-//       ],
-//       ["clean"],
-//       [{ color: [] }],
-//     ],
-//   };
+    setNewBoilerplateFields((fields) => ({
+      ...fields,
+      [field]: newValue,
+    }));
+  };
 
-//   const formats = [
-//     "header",
-//     "bold",
-//     "italic",
-//     "underline",
-//     "strike",
-//     "blockquote",
-//     "list",
-//     "bullet",
-//     "indent",
-//     "color",
-//   ];
+  const handleCancel = (event) => {
+    event.preventDefault();
+    props.onCancel();
+  };
 
-//   return (
-//     <Form className="SectionForm" onSubmit={handleSubmit}>
-//       <Form.Group>
-//         <Form.Label>Category</Form.Label>
-//         <Form.Control
-//           as="select"
-//           name="categoryId"
-//           value={categoryId}
-//           onChange={(event) => setCategoryId(event.target.value)}
-//           required
-//         >
-//           <option value="" disabled>
-//             Select Category
-//           </option>
-//           {categories.map((category) => {
-//             return (
-//               <option
-//                 key={category.id}
-//                 value={category.id}
-//                 onChange={(event) => setCategoryId(event.target.value)}
-//               >
-//                 {category.name}
-//               </option>
-//             );
-//           })}
-//         </Form.Control>
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label>Boilerplate Title</Form.Label>
-//         <Form.Control
-//           type="text"
-//           value={boilerplateFields.title}
-//           onChange={(event) => {
-//             setBoilerplateFields({
-//               ...boilerplateFields,
-//               title: event.target.value,
-//             });
-//           }}
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label>Boilerplate Text</Form.Label>
-//         <ReactQuill
-//           className="SectionForm__ContentEditor"
-//           ref={quillEl}
-//           value={boilerplateFields.html}
-//           onChange={(html) => {
-//             setBoilerplateFields({
-//               ...boilerplateFields,
-//               html,
-//             });
-//           }}
-//         />
-//       </Form.Group>
-//       <div className="SectionForm__Actions">
-//         <Link>Store Section as Boilerplate</Link>
-//         <Button variant="outline-dark" size="lg" onClick={handleCancel}>
-//           Cancel
-//         </Button>
-//         <Button variant="dark" size="lg" type="submit">
-//           Save
-//         </Button>
-//       </div>
-//     </Form>
-//   );
-// }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createBoilerplate(organizationClient, newBoilerplateFields)
+      .then((boilerplate) => {
+        if (boilerplate) {
+          alert("Section saved as boilerplate!");
+          // handle close save as boilerplate modal
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(
+          "Oh no! Something went wrong when you were trying to save the section as boilerplate."
+        );
+      });
+  };
+
+  useEffect(() => {
+    if (!organizationClient) {
+      return;
+    }
+
+    getAllCategories(organizationClient).then((categories) => {
+      setCategories(categories);
+    });
+  }, [organizationClient]);
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["clean"],
+      [{ color: [] }],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "color",
+  ];
+
+  return (
+    <Form className="SectionForm" onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          as="select"
+          name="category_id"
+          value={newBoilerplateFields.category_id}
+          onChange={handleChangeField("category_id")}
+          required
+        >
+          <option value="" disabled>
+            Select Category
+          </option>
+          {categories.map((category) => {
+            return (
+              <option
+                key={category.id}
+                value={category.id}
+                onChange={(event) => {
+                  setNewBoilerplateFields({
+                    ...newBoilerplateFields,
+                    category_id: event.target.value,
+                  });
+                }}
+              >
+                {category.name}
+              </option>
+            );
+          })}
+        </Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Boilerplate Title</Form.Label>
+        <Form.Control
+          type="text"
+          value={newBoilerplateFields.title}
+          onChange={(event) => {
+            setNewBoilerplateFields({
+              ...newBoilerplateFields,
+              title: event.target.value,
+            });
+          }}
+          required
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Boilerplate Text</Form.Label>
+        <ReactQuill
+          className="SectionForm__ContentEditor"
+          ref={quillEl}
+          value={newBoilerplateFields.text}
+          onChange={(event) => {
+            setNewBoilerplateFields({
+              ...newBoilerplateFields,
+              text: event.target.value,
+              wordcount: countWords(event.target.value),
+            });
+          }}
+        />
+      </Form.Group>
+      <div className="SectionForm__Actions">
+        <Button variant="outline-dark" size="lg" onClick={handleCancel}>
+          Cancel
+        </Button>
+        <Button variant="dark" size="lg" type="submit">
+          Save
+        </Button>
+      </div>
+    </Form>
+  );
+}
--- src/Components/SectionsShow.js
+++ src/Components/SectionsShow.js
@@ -2,26 +2,26 @@ import React, { useState, useEffect, useCallback } from "react";
 import countSectionWords from "../Helpers/countSectionWords";
 import "./SectionsShow.css";
 import { Button } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
 import Modal from "./Elements/Modal";
 import SaveSectionAsBoilerplate from "./Sections/SaveSectionAsBoilerplate";
 
 export default function SectionsShow(props) {
   const { section } = props;
-  const [
-    showSaveSectionAsBoilerplateModal,
-    setShowSaveSectionAsBoilerplateModal,
-  ] = useState(false);
-  const handleShowSaveSectionAsBoilerplateModal = (event) =>
-    setShowSaveSectionAsBoilerplateModal(true);
-  const handleCloseSaveSectionAsBoilerplateModal = (event) =>
-    setShowSaveSectionAsBoilerplateModal(false);
+  const [showSaveAsBoilerplateModal, setShowSaveAsBoilerplateModal] =
+    useState(false);
+  const handleShowSaveAsBoilerplateModal = (event) => {
+    setShowSaveAsBoilerplateModal(true);
+    console.log(`modal should render: ${showSaveAsBoilerplateModal}`);
+    console.log("hi!");
+  };
+  const handleCloseSaveAsBoilerplateModal = (event) =>
+    setShowSaveAsBoilerplateModal(false);
 
   return (
     <>
       <div className="Section__Header">
         <h1 className="Section__Title">{section.title}</h1>
-        <Button type="submit" onClick={handleShowSaveSectionAsBoilerplateModal}>
+        <Button onClick={handleShowSaveAsBoilerplateModal}>
           Save Section as Boilerplate
         </Button>
         <p>Word count: {countSectionWords(section)}</p>
@@ -29,14 +29,10 @@ export default function SectionsShow(props) {
       <div dangerouslySetInnerHTML={{ __html: section.text }}></div>
       <Modal
         className="modal-popup"
-        onClose={handleCloseSaveSectionAsBoilerplateModal}
-        show={showSaveSectionAsBoilerplateModal}
+        onClose={handleCloseSaveAsBoilerplateModal}
+        show={showSaveAsBoilerplateModal}
       >
-        <Card>
-          <Card.Body>
-            {/* <SaveSectionAsBoilerplate section={section} /> */}
-          </Card.Body>
-        </Card>
+        <SaveSectionAsBoilerplate section={section} />
       </Modal>
     </>
   );
