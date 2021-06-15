80c11dd15a932e8bd1570e94ef38a4426752a3ad
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-06-15 17:42:22 -0500

    updated dependencies to run locally

---
+++ src/config/ReactQuillConfig.js
@@ -0,0 +1,27 @@
+export const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["clean"],
+    [{ color: [] }],
+  ],
+};
+
+export const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "color",
+];
