fc02a7d747f6a9a6e213bb4e317324cb2a577375
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-06-16 20:50:42 -0500

    finished button reusable design component, added modal design component, started adding Form design component

---
+++ src/Components/design/Form/Form.js
@@ -0,0 +1,11 @@
+//  Forms contain:
+//      Header text
+//      Label texts for inputs
+//      Inputs:
+//          Text inputs
+//          Quilltext html inputs
+//          Dropdowns
+//          Checkboxes
+//      Buttons:
+//          Cancel button - outline
+//          Save button - primary
