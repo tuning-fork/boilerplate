a6a00314c583625fc9ebc1120ccf466cc8cac668
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-06-17 20:39:41 -0500

    Make adjustments to Modal component
    * Use dialog instead of div
    * Use createPortal to render modal in document.body
    * Add heading to modal with configurable text and className
    * Move background blur (overlay) to outside dialog

--- src/Components/design/Modal/Modal.js
+++ src/Components/design/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { createPortal } from "react";
 import PropTypes from "prop-types";
+import clsx from "clsx";
 import "./Modal.css";
 
 export default function Modal(props) {
   if (!props.show) {
     return null;
   }
-  return (
-    <div className="modal">
-      <div className=" modal__content">{props.children}</div>
-    </div>
+  return createPortal(
+    <>
+      <dialog className="modal" aria-labelledby="modal-heading" aria-modal open>
+        <h1
+          id="modal-heading"
+          className={clsx(props.headingClassName, "modal__heading")}
+        >
+          {props.heading}
+        </h1>
+        {props.children}
+      </dialog>
+      <div className="modal-overlay"></div>
+    </>,
+    document.body
   );
 }
 
 Modal.propTypes = {
   show: PropTypes.bool,
+  heading: PropTypes.string.isRequired,
+  headingClassName: PropTypes.string,
 };
 
 Modal.defaultProps = {
--- src/Components/design/Modal/Modal.stories.jsx
+++ src/Components/design/Modal/Modal.stories.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
+import Button from "../Button/Button";
 import "../theme.css";
 
 export default {
   title: "Design/Modal",
   component: Modal,
   argTypes: {
+    heading: {
+      defaultValue: "Hello!",
+    },
     show: {
       defaultValue: true,
     },
@@ -14,7 +18,20 @@ export default {
 
 export const ModalRegular = (props) => (
   <Modal {...props}>
-    <h1>Hello!</h1>
     <p>Welcome to the Modal! Enjoy your stay</p>
   </Modal>
 );
+
+export const ModalShowing = () => {
+  const [showingModal, setShowingModal] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setShowingModal(true)}>Open Modal</Button>
+      <Modal heading="Hello!" show={showingModal}>
+        <p>Welcome to the Modal! Enjoy your stay</p>
+        <Button onClick={() => setShowingModal(false)}>Close</Button>
+      </Modal>
+    </>
+  );
+};
