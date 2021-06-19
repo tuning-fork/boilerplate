d0e3bb303584d9334c170948d74d6dfd334b16d5
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-06-19 10:49:23 -0500

    revised reusable design components input files - text and checkbox

--- src/Components/design/Input/Input.js
+++ src/Components/design/Input/Input.js
@@ -9,52 +9,30 @@ import PropTypes from "prop-types";
 import clsx from "clsx";
 import "./Input.css";
 
-export const InputVariant = {
+export const InputType = {
   Text: "text",
-  QuillText: "quilltext",
   Checkbox: "checkbox",
-  Dropdown: "dropdown",
+  // QuillText: "quilltext",
+  // Dropdown: "dropdown",
 };
 
 export default function Input(props) {
   return (
-    <button
-      {...props}
-      className={clsx(
-        props.className,
-        "input",
-        `input--${props.variant}`,
-        props.disabled && "input--disabled"
-      )}
-    >
-      {Array.isArray(props.children)
-        ? props.children.map((child, index) => {
-            //We might be able to use this for placeholder icons?
-            // Wrap text nodes in span for styling with icons.
-            if (typeof child === "string") {
-              return <span key={index}>{child}</span>;
-            }
-            return child;
-          })
-        : props.children}
-    </button>
+    <label>
+      {props.labelText}
+      <input {...props} className={clsx(props.className, "input")} />
+    </label>
   );
 }
 
 Input.propTypes = {
   className: PropTypes.string,
-  //It seems like we want disabled inputs, maybe?
-  disabled: PropTypes.bool,
-  type: PropTypes.oneOf(["button", "reset", "submit"]),
-  variant: PropTypes.oneOf(Object.values(ButtonVariant)),
+  labelText: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(Object.values(InputType)),
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
-Button.defaultProps = {
-  className: "",
-  color: ButtonColor.Primary,
-  disabled: false,
-  onClick: () => {},
-  tabIndex: 0,
-  type: "input",
-  variant: InputVariant.Text,
+Input.defaultProps = {
+  type: "text",
 };
--- src/Components/design/Input/Input.stories.js
+++ src/Components/design/Input/Input.stories.js
@@ -1,117 +1,103 @@
 import React from "react";
 import Input, { InputType } from "./Input";
-import "../theme.css";
+import "../../design.css";
 
 export default {
   title: "Design/Input",
   component: Input,
   argTypes: {
-    text: {
-      defaultValue: "Button",
-      control: {
-        type: "text",
-      },
+    value: {
+      defaultValue: "Jenny",
+      // control: { type: "text" },
     },
-    variant: {
-      options: Object.values(ButtonVariant),
-      control: { type: "radio" },
+    labelText: {
+      defaultValue: "Name",
+      control: { type: "text" },
     },
-    color: {
-      options: Object.values(ButtonColor),
+    type: {
+      options: Object.values(InputType),
       control: { type: "radio" },
     },
   },
 };
 
-const Container = ({ children }) => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-evenly",
-      marginBottom: "20px",
-    }}
-  >
-    {children}
-  </div>
-);
+export const InputBasic = (props) => <Input {...props} />;
 
-export const InputText = (props) => <Input {...props}
-type="text"
-            value={props.value}
-            name={props.name}
-            // onChange={(event) => setNewTitle(event.target.value)}
-            required>
-            </Input>;
+// export const InputText = (props) => <Input {...props}
+// type="text"
+//             value={props.value}
+//             name={props.name}
+//             // onChange={(event) => setNewTitle(event.target.value)}
+//             required>
+//             </Input>;
 
+// export const InputText = (props) => <Input {...props}>
+//    <label>{props.label}</label>
+//    <input type={props.type}
+// value={props.value}
+// name={props.name}
+// required></input>
+// </Input>
+// ;
 
+// export const InputQuillText = (props) => (
 
-export const InputText = (props) => <Input {...props}>
-   <label>{props.label}</label> 
-   <input type={props.type}
-value={props.value}
-name={props.name}
-required></input>
-</Input>
-;
+// );
 
-export const InputQuillText = (props) => (
-  
-);
+// export const InputCheckbox = (props) => (
+//     <Input {...props}
+//     type={props.type}>
+//     <label>{props.label}</label>
+//           <checkbox
+//             type={props.type}
+//             name={props.name}
+//             checked={props.checked}
+//             // onChange={(event) => setNewSubmitted(event.target.checked)}
+//           />
+//     </Input>
+// );
 
-export const InputCheckbox = (props) => (
-    <Input {...props}
-    type={props.type}>
-    <label>{props.label}</label>
-          <checkbox
-            type={props.type}
-            name={props.name}
-            checked={props.checked}
-            // onChange={(event) => setNewSubmitted(event.target.checked)}
-          />
-    </Input>
-);
+// export const InputDropdown = (props) => (
+//   <Input {...props}
+//   type={props.type}>
+//   <label>{props.label}</label>
+//   <input
+//             as="select"
+//             name={props.name}
+//             value={props.value}
+//             // onChange={(event) => setNewCategoryId(event.target.value)}
+//             required
+//        >
+//     {props.options.map = (option) => {
+//         return (
+//         <select
+//         name={option.name}
+//         value={option.id}
+//         key={option.id}
+//         >
+//         {option.name}
+//         </select>
+//         )
+//     }}
+//     </input>
+//   </Input>
+// );
 
-export const InputDropdown = (props) => (
-  <Input {...props} 
-  type={props.type}>
-  <label>{props.label}</label>
-  <input
-            as="select"
-            name={props.name}
-            value={props.value}
-            // onChange={(event) => setNewCategoryId(event.target.value)}
-            required
-       >   
-    {props.options.map = (option) => {
-        return (
-        <select 
-        name={option.name}
-        value={option.id}
-        key={option.id}
-        >
-        {option.name}
-        </select>
-        )
-    }}
-    </input>
-  </Input>
-);
+// <div>
+//   <form action="/action_page.php">
+//     <label for="fname">First Name</label>
+//     <input type="text" id="fname" name="firstname" placeholder="Your name..">
 
-<div>
-  <form action="/action_page.php">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+//     <label for="lname">Last Name</label>
+//     <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+//     <label for="country">Country</label>
+//     <select id="country" name="country">
+//       <option value="australia">Australia</option>
+//       <option value="canada">Canada</option>
+//       <option value="usa">USA</option>
+//     </select>
 
-    <label for="country">Country</label>
-    <select id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-  
-    <input type="submit" value="Submit">
-  </form>
-</div>
+//     <input type="submit" value="Submit">
+//   </form>
+// </div>
