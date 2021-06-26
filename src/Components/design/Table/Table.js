a21f666bd0b06da349dc59b81a30baed773b3394
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-06-26 16:51:58 -0500

    Align numeric cells to the right

--- src/Components/design/Table/Table.js
+++ src/Components/design/Table/Table.js
@@ -7,39 +7,60 @@ import "./Table.css";
 export default function Table(props) {
   const columns = useMemo(() => props.columns, [props.columns]);
   const data = useMemo(() => props.data, [props.data]);
+
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data });
 
-  return (
-    <table {...getTableProps()} className={clsx(props.className, "table")}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()} className="table__header">
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
+  const renderHeader = (column) => {
+    const isNumber = typeof column.accessor(data[0]) === "number";
+
+    return (
+      <th
+        {...column.getHeaderProps()}
+        className={clsx(
+          console.dir(column),
+          "table__header",
+          isNumber && "table__header--number"
+        )}
+      >
+        {column.render("Header")}
+      </th>
+    );
+  };
+
+  const renderHeaderGroup = (headerGroup) => {
+    return (
+      <tr {...headerGroup.getHeaderGroupProps()}>
+        {headerGroup.headers.map(renderHeader)}
+      </tr>
+    );
+  };
+
+  const renderRow = (row) => {
+    prepareRow(row);
+
+    return (
+      <tr {...row.getRowProps()}>
+        {row.cells.map((cell) => {
+          const isNumber = typeof cell.value === "number";
 
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td {...cell.getCellProps()} class="table__cell">
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
+            <td
+              {...cell.getCellProps()}
+              className={clsx("table__cell", isNumber && "table__cell--number")}
+            >
+              {cell.render("Cell")}
+            </td>
           );
         })}
-      </tbody>
+      </tr>
+    );
+  };
+
+  return (
+    <table {...getTableProps()} className={clsx(props.className, "table")}>
+      <thead>{headerGroups.map(renderHeaderGroup)}</thead>
+      <tbody {...getTableBodyProps()}>{rows.map(renderRow)}</tbody>
     </table>
   );
 }
--- src/Components/design/Table/Table.stories.js
+++ src/Components/design/Table/Table.stories.js
@@ -12,11 +12,25 @@ export const Table = (props) => (
     {...props}
     columns={[
       { Header: "Status", accessor: "status" },
+      { Header: "Number", accessor: "number" },
       { Header: "Title", accessor: "title" },
     ]}
     data={[
-      { status: "Draft", title: "Good Place Neighborhood Grant" },
-      { status: "Submitted", title: "Bad Place Neighborhood Grant" },
+      {
+        status: "Draft",
+        title: "Good Place Neighborhood Grant",
+        number: 3331.296,
+      },
+      {
+        status: "Submitted",
+        title: "Bad Place Neighborhood Grant",
+        number: 1012.997,
+      },
+      {
+        status: "Archived",
+        title: "Medium Place Neighborhood Grant",
+        number: 3079.296,
+      },
     ]}
   />
 );
