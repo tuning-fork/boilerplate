db6c88e25954e9b0f5f74ec523e88d9344e7fa76
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-07-03 15:03:57 -0500

    Rename Input to TextBox and support prefix and suffix

---
+++ src/Components/design/TextBox/TextBox.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import "./TextBox.css";
+
+export default function TextBox(props) {
+  const { labelText, className, prefix, suffix, ...restProps } = props;
+
+  return (
+    <label className={clsx(className, "text-box")}>
+      <span className="text-box__label">{labelText}</span>
+      <span className="text-box__container">
+        {prefix && <span className="text-box__prefix">{prefix}</span>}
+        <input {...restProps} className="text-box__input" />
+        {suffix && <span className="text-box__suffix">{suffix}</span>}
+      </span>
+    </label>
+  );
+}
+
+TextBox.propTypes = {
+  className: PropTypes.string,
+  labelText: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  prefix: PropTypes.node,
+  suffix: PropTypes.node,
+  value: PropTypes.string,
+};
+
+TextBox.defaultProps = {};
