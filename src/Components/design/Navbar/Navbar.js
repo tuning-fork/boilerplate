2e7cf93b7ddb22e4bf84eb0a16e54096d9b69e1c
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-07-19 17:52:16 -0500

    finalized navbar

--- src/Components/design/Navbar/Navbar.js
+++ src/Components/design/Navbar/Navbar.js
@@ -3,25 +3,26 @@ import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import Button from "../Button/Button";
-//Will need a reusable user icon
-//Will need a logotype svg
-
+import { ReactComponent as Logo } from "./BOILERPLATE.svg";
 import "./Navbar.css";
 
 export default function Navbar(props) {
   return (
     <div className={clsx(props.className, "navbar")}>
-      <NavLink
-        className="navbar__navitem orgselectlink"
-        activeClassName="navbar__navitem--selected"
-        to={`/organizations/${props.organizationId}/dashboard`}
-      >
-        All Organizations
-      </NavLink>
-      <div className="navbar__navitem"> | </div>
-      <div className="navbar__navitem">Baklava Foundation</div>
-      <Button className="button--usericon">JW</Button>
-      {/* <div className="userlogo"></div> */}
+      <Logo className="navbar__logo" />
+      <div className="navbar__links">
+        <NavLink
+          to={`/organizations/${props.organizationId}/dashboard`}
+          className="navbar__all-organizations"
+        >
+          {"<"} All Organizations
+        </NavLink>
+        <span className="navbar__divider"> | </span>
+        <span className="navbar__current-organization">Baklava Foundation</span>
+        <Button className="button" variant="usericon">
+          JW
+        </Button>
+      </div>
     </div>
   );
 }
