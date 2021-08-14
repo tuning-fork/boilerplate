758e3e51bd6ad872c66cf69a04a9baa8adc24e5a
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-08-14 15:31:01 -0500

    Create Dropdown component

---
+++ src/Components/design/Dropdown/Dropdown.stories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Component from "./Dropdown";
+
+export default {
+  title: "Design/Dropdown",
+  component: Component,
+  argTypes: {
+    labelText: {
+      defaultValue: "Further Actions",
+    },
+    placeholder: {
+      defaultValue: "Pick One",
+    },
+    options: {
+      defaultValue: [
+        { value: "MARK_AS_SUBMITTED", label: "Mark as Submitted" },
+        { value: "MARK_AS_SUCCESSFUL", label: "Mark as Successful" },
+        { value: "MARK_AS_COPY", label: "Mark as Copy" },
+        { value: "ARCHIVE", label: "Archive" },
+      ],
+    },
+  },
+};
+
+export const Dropdown = (props) => <Component {...props} />;
---
+++ src/Components/design/Dropdown/DropdownContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const DropdownContext = createContext();
+
+export default DropdownContext;
