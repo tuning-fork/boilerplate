41469006da7cef377283aeaef579febe9e67c2a1
Jess-White <58121322+Jess-White@users.noreply.github.com>
2021-10-21 17:16:05 -0500

    Update src/Components/PasteBoilerplateContentPopout/PasteBoilerplateContentPopoutContext.js
    Co-authored-by: Cain Watson <23223956+cainwatson@users.noreply.github.com>

--- src/Components/PasteBoilerplateContentPopout/PasteBoilerplateContentPopoutContext.js
+++ src/Components/PasteBoilerplateContentPopout/PasteBoilerplateContentPopoutContext.js
@@ -1,34 +1,3 @@
-// import React, { createContext, useCallback, useState } from "react";
-
-// export const PasteBoilerplateContentPopoutContext = createContext();
-
-// export const PasteBoilerplateContentPopoutProvider = ({ children }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [subscriber, setSubscriber] = useState();
-
-//   const onPasteBoilerplate = useCallback(
-//     (newSubscriber) => setSubscriber(() => newSubscriber),
-//     [setSubscriber]
-//   );
-//   const unsubscribeBoilerplate = useCallback(() => {
-//     setSubscriber(null);
-//   }, [setSubscriber]);
-//   const context = {
-//     isOpen,
-//     setIsOpen,
-//     subscriber,
-//     onPasteBoilerplate,
-//     pasteBoilerplate: subscriber || (() => {}),
-//     unsubscribeBoilerplate,
-//   };
-
-//   return (
-//     <PasteBoilerplateContentPopoutContext.Provider value={context}>
-//       {children}
-//     </PasteBoilerplateContentPopoutContext.Provider>
-//   );
-// };
-
 import React, { createContext, useCallback, useState } from "react";
 
 export const PasteBoilerplateContentPopoutContext = createContext();
