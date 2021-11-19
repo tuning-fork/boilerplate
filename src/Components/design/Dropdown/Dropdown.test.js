08ece1877051a3c1edcadda54d633f95d240cbaf
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-11-18 18:07:07 -0600

    added Dropdown tests

--- src/Components/design/Dropdown/Dropdown.test.js
+++ src/Components/design/Dropdown/Dropdown.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Dropdown from "./Dropdown";
 
@@ -25,11 +25,25 @@ describe("Dropdown", () => {
   it("displays the list of options in dropdown menu", () => {
     render(<Dropdown {...defaultProps} placeholder={"banana"} />);
     const button = screen.getByText("banana");
+
     expect(screen.queryByRole("listbox")).not.toBeInTheDocument();
     expect(screen.getByText("Archive")).not.toBeVisible();
+
     userEvent.click(button);
+
     expect(screen.getByRole("listbox")).toBeInTheDocument();
     expect(screen.getByText("Archive")).toBeVisible();
+  });
+
+  it("opens the dropDown when enter is pressed", () => {
+    render(<Dropdown {...defaultProps} placeholder={"banana"} />);
+
+    fireEvent.keyDown(screen.getByText("banana"), {
+      key: "Enter",
+      code: "Enter",
+    });
+
+    expect(screen.getByRole("listbox")).toBeInTheDocument();
   });
 
   it("displays a selected option", () => {
@@ -42,8 +56,39 @@ describe("Dropdown", () => {
   it("calls onChange on select", () => {
     render(<Dropdown {...defaultProps} placeholder={"banana"} />);
     const button = screen.getByText("banana");
+
     userEvent.click(button);
     userEvent.click(screen.getByText("Archive"));
+
     expect(onChange).toHaveBeenCalled();
+  });
+
+  it("closes the dropDown when handleDocumentClick is clicked", () => {
+    render(<Dropdown {...defaultProps} placeholder={"banana"} />);
+    userEvent.click(screen.getByText("banana"));
+
+    expect(screen.getByRole("listbox")).toBeInTheDocument();
+
+    act(() => {
+      userEvent.click(document.body);
+    });
+
+    expect(screen.queryByRole("listbox")).not.toBeInTheDocument();
+  });
+
+  it("closes the dropDown when escape is pressed", () => {
+    render(<Dropdown {...defaultProps} placeholder={"banana"} />);
+    const button = screen.getByText("banana");
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("listbox")).toBeInTheDocument();
+
+    fireEvent.keyDown(button, {
+      key: "Escape",
+      code: "Escape",
+    });
+
+    expect(screen.queryByRole("listbox")).not.toBeInTheDocument();
   });
 });
