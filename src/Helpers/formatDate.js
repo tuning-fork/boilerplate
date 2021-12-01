75041e55072dacb007081e46c8665124690a205a
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-11-30 22:18:22 -0600

    Allow formatDate helper to take in date instance

--- src/Helpers/formatDate.js
+++ src/Helpers/formatDate.js
@@ -1,6 +1,9 @@
 import { format, parseISO } from "date-fns";
 
 const formatDate = (dateString) => {
+  if (dateString instanceof Date) {
+    return format(dateString, "PPP");
+  }
   return format(parseISO(dateString), "PP");
 };
 
