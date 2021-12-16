413954f1828f8bb42577652d149fe887c80a2ada
Mike McFaddin <52680887+Michael-McFaddin@users.noreply.github.com>
2021-12-16 10:03:02 -0500

    added splashpage background image and get started button, updated splashpage css

--- src/Components/Layouts/SplashpageLayout/SplashpageLayout.js
+++ src/Components/Layouts/SplashpageLayout/SplashpageLayout.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarSplashpage from "../../design/Navbar/NavbarSplashpage/NavbarSplashpage";
 import "./SplashpageLayout.css";
+import allSplashpageGraphicElements from "./allSplashpageGraphicElements.png";
+import getStartedButton from "./getStartedButton.png";
+import NavbarLogin from "../../design/Navbar/NavbarLogin/NavbarLogin";
+import Button from "../../design/Button/Button";
 
 export default function SplashpageLayout(props) {
+  const [showNavbarLogin, setShowNavbarLogin] = useState(false);
+
   return (
-    <main className="layout">
+    <main className="splashpage-layout">
       <NavbarSplashpage />
-      <div className="layout__content">{props.children}</div>
+      <div className="splashpage-layout__content">{props.children}</div>
+      <img
+        src={allSplashpageGraphicElements}
+        alt="Splashpage graphics"
+        className="splashpage-layout__graphic-elements"
+      />
+      <div className="splashpage-layout__bottom-buttons">
+        <Button variant="none">
+          <img src={getStartedButton} alt="Get started graphic button" />
+        </Button>
+        <Button variant="none">or Sign In</Button>
+      </div>
+      {showNavbarLogin ? <NavbarLogin /> : null}
     </main>
   );
 }
