581128790dabbd79cde1ee11078ccfa58dfc1dc7
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-12-19 16:12:56 -0600

    Remove unused import

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import TextBox from "./design/TextBox/TextBox";
 import Button from "./design/Button/Button";
