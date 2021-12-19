28435d899d1ac07003a2fd78e351150256626d7d
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-12-19 16:12:49 -0600

    Prepend REACT_APP_ to api url override

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const DEVELOPMENT_API_URL = process.env.API_URL || "http://localhost:3000";
+const DEVELOPMENT_API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3000";
 const BASE_URL =
   process.env.NODE_ENV === "development" ? DEVELOPMENT_API_URL : "";
 
