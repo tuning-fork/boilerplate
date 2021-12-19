08033c17b9626c800566ade1046da9179e130e94
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-12-19 15:48:32 -0600

    Create file for api client

---
+++ src/config/apiClient.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const DEVELOPMENT_API_URL = process.env.API_URL || "http://localhost:3000";
+const BASE_URL =
+  process.env.NODE_ENV === "development" ? DEVELOPMENT_API_URL : "";
+
+const apiClient = axios.create({
+  baseURL: `${BASE_URL}/api/`,
+});
+
+export default apiClient;
