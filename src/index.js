e224e5067bed28defd8d0dcc28534603fe3ea580
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-12-19 16:48:09 -0600

    Remove setting axios base url in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { NetworkErrorBoundary, CacheProvider } from "rest-hooks";
-import axios from "axios";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { PasteBoilerplateContentPopoutProvider } from "./Components/PasteBoilerplateContentPopout/PasteBoilerplateContentPopoutContext";
 import Spinner from "./Components/Helpers/Spinner";
 import "./Components/design.css";
-
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000/" : "/";
 
 ReactDOM.render(
   <React.StrictMode>
