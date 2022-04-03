6d6b8ad753b603d6cc45b01c8bf69609a5ea610f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-04-03 16:02:38 -0500

    refactored reports service to match grants service.

--- src/Services/Organizations/Grants/GrantReportsService.js
+++ src/Services/Organizations/Grants/GrantReportsService.js
@@ -1,8 +1,32 @@
+const mapGrantReport = (apiGrantReport) => ({
+  archived: apiGrantReport.archived,
+  createdAt: new Date(apiGrantReport.created_at),
+  deadline: new Date(apiGrantReport.deadline),
+  fundingOrgId: apiGrantReport.grant.funding_org_id.toString(),
+  fundingOrgName: apiGrantReport.grant.funding_org_name,
+  id: apiGrantReport.id.toString(),
+  organizationId: apiGrantReport.organization_id.toString(),
+  purpose: apiGrantReport.grant.purpose,
+  rfpUrl: apiGrantReport.grant.rfp_url,
+  title: apiGrantReport.title,
+  updatedAt: new Date(apiGrantReport.updated_at),
+  reportSections: apiGrantReport.report_sections
+    ? apiGrantReport.report_sections.map(mapSection)
+    : [],
+});
+
+const mapGrantReportToApiGrantReport = (grantReport) => ({
+  ...grantReport,
+  rfp_url: grantReport.rfpUrl,
+  funding_org_id: grantReport.fundingOrgId,
+  organization_id: grantReport.organizationId,
+});
+
 // getGrantReport
 export const getGrantReport = (organizationClient, grantId, reportId) => {
   return organizationClient
     .get(`/grants/${grantId}/reports/${reportId}`)
-    .then((response) => response.data);
+    .then((response) => mapGrantReport(response.data));
 };
 
 // listGrantReports
@@ -10,7 +34,7 @@ export const getGrantReport = (organizationClient, grantId, reportId) => {
 export const getAllGrantReports = (organizationClient, grantId) => {
   return organizationClient
     .get(`/grants/${grantId}/reports/`)
-    .then((response) => response.data);
+    .then((response) => response.data.map(mapGrantReport));
 };
 
 // deleteGrantReport
@@ -23,9 +47,16 @@ export const deleteGrantReport = (organizationClient, grantId, reportId) => {
 
 // createGrantReport
 
-export const createGrantReport = (organizationClient, grantId, newReport) => {
+export const createGrantReport = (
+  organizationClient,
+  grantId,
+  newGrantReport
+) => {
   return organizationClient
-    .post(`/grants/${grantId}/reports/`, newReport)
+    .post(
+      `/grants/${grantId}/reports/`,
+      mapGrantReportToApiGrantReport(newGrantReport)
+    )
     .then((response) => response.data);
 };
 
@@ -38,6 +69,25 @@ export const updateGrantReport = (
   fieldsToUpdate
 ) => {
   return organizationClient
-    .patch(`/grants/${grantId}/reports/${reportId}`, fieldsToUpdate)
+    .patch(
+      `/grants/${grantId}/reports/${reportId}`,
+      mapGrantReportToApiGrantReport(fieldsToUpdate)
+    )
+    .then((response) => response.data);
+};
+
+// copyGrantReport
+
+export const copyGrantReport = (
+  organizationClient,
+  grantId,
+  reportId,
+  copyGrantReportFields
+) => {
+  return organizationClient
+    .post(
+      `/grants/${grantId}/reports/${reportId}/copy`,
+      mapGrantReportToApiGrantReport(copyGrantReportFields)
+    )
     .then((response) => response.data);
 };
