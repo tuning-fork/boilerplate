2fc14c14b6e1340398d04579589e8f9606c4f70a
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-04-16 11:11:46 -0500

    added Explainer directory and components

---
+++ src/Components/Explainer/ExplainerTexts.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export const ExplainerTexts = [
+  {
+    title: "What Is Boilerplate?",
+    text: "",
+  },
+  {
+    title: "Why Use Boilerplate?",
+    text: "",
+  },
+  {
+    title: "Create Content",
+    text: "",
+  },
+  {
+    title: "Build Content Library",
+    text: "",
+  },
+  {
+    title: "Draft Grants for Review",
+    text: "",
+  },
+  {
+    title: "Organize Grants",
+    text: "",
+  },
+  {
+    title: "Export Grants",
+    text: "",
+  },
+  {
+    title: "Custom Templates",
+    text: "",
+  },
+  {
+    title: "Analyze Grant Submissions",
+    text: "",
+  },
+  {
+    title: "Create Reports",
+    text: "",
+  },
+  {
+    title: "Boilerplate Team",
+    text: "",
+  },
+];
