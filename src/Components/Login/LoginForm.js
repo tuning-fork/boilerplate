dd220bedce7704deeabd8d2a70a1a6272bfb5413
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-04-24 16:15:55 -0500

    cleaned up LoginForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -10,17 +10,10 @@ export default function LoginForm(props) {
   const location = useLocation();
   const { error } = useCurrentUser();
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   setLoginFields({ ...loginFields });
-  //   props.onSubmit(loginFields);
-  // };
-
   return (
     <>
       {location.state?.loggedOut && <p>Successfully logged out!</p>}
       {error && <p className="login__error">Error: {error.message}</p>}
-      {/* <form onSubmit={handleSubmit} className="login-form"> */}
       <form className="login-form">
         <TextBox
           type="email"
