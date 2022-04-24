9effbb6919a37dcc27591d3044cf889e1264b4ed
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-04-24 16:15:16 -0500

    cleaned up login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,53 +1,20 @@
-// import React, { useCallback } from "react";
 import React from "react";
-// import { useHistory, useLocation } from "react-router-dom";
 import Container from "../design/Container/Container";
-// import { useCurrentUser } from "../../Contexts/currentUserContext";
 import LoginForm from "./LoginForm";
 import NavbarLoginForm from "./NavbarLoginForm";
 import "./Login.css";
 
 export default function Login(props) {
-  // const history = useHistory();
-  // const location = useLocation();
-  // const { login } = useCurrentUser();
-
-  // const handleCancel = (event) => {
-  //   event.preventDefault();
-  //   props.onCancel();
-  //   history.push(`/splashpage`);
-  // };
-
-  // const handleSubmit = useCallback(
-  //   async ({ email, password }) => {
-  //     // event.preventDefault();
-  //     await login(email, password);
-  //     // alert("You're signed in!");
-  //     history.push(location.state?.from ?? "/organizations");
-  //   },
-  //   [history, location, login]
-  // );
-
   return (
     <>
-      {/* <h1 className="login">Login</h1> */}
       {props.formType === "standard" ? (
         <div className="login">
           <Container as="section" centered>
-            <LoginForm
-              // onSubmit={handleSubmit}
-              // onCancel={handleCancel}
-              toggleModalContents={props.toggleModalContents}
-            />
+            <LoginForm toggleModalContents={props.toggleModalContents} />
           </Container>
         </div>
       ) : (
-        <NavbarLoginForm
-          // onSubmit={handleSubmit}
-          // onCancel={handleCancel}
-          // handleModal={props.handleModal}
-          toggleModalContents={props.toggleModalContents}
-        />
+        <NavbarLoginForm toggleModalContents={props.toggleModalContents} />
       )}
     </>
   );
