bff860f3e730aa7e16aaef05462fb66191d14d93
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-05-30 18:39:14 -0500

    added search input

--- src/Components/ReviewerList/ReviewerList.js
+++ src/Components/ReviewerList/ReviewerList.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useContext, useMemo } from "react";
 import { useQuery } from "react-query";
+import TextBox from "../design/TextBox/TextBox";
 import "./ReviewerList.css";
 import ReviewerListItem from "./ReviewerListItem";
 import { getAllOrganizationUsers } from "../../Services/OrganizationService";
@@ -10,14 +11,30 @@ export default function ReviewerList() {
   const { data: reviewers } = useQuery("getAllOrganizationUsers", () =>
     getAllOrganizationUsers(organizationClient)
   );
+  const [requestedReviewers, setRequestedReviewers] = useState([]);
+  const [searchFilters, setSearchFilters] = useState({
+    name: "",
+  });
+  const filteredReviewers = useMemo(() => {
+    return reviewers.filter((reviewer) => {
+      console.log(reviewer);
+    });
+  }, [reviewers, searchFilters]);
 
   return (
     <aside className="reviewer-list">
       <header className="reviewer-list__header">
         <h2 className="heading-4">Select Reviewer</h2>
       </header>
+
+      <TextBox
+        labelText="Search"
+        onChange={(event) =>
+          setSearchFilters({ ...searchFilters, text: event.target.value })
+        }
+      />
       <ul className="reviewer-list__reviewers-index">
-        {reviewers.map((reviewer) => (
+        {filteredReviewers.map((reviewer) => (
           <ReviewerListItem key={reviewer.id} reviewer={reviewer} />
         ))}
       </ul>
