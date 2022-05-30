fd63d0aee7d6100ddb4826dd11ee6342ab36c37f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-05-30 12:24:01 -0500

    added Panel component WIP for splashpage explainers

--- src/Components/design/Panel/Panel.js
+++ src/Components/design/Panel/Panel.js
@@ -1,41 +1,38 @@
 import React from "react";
-import { createPortal } from "react-dom";
+// import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import "./Panel.css";
+import PanelCancelButton from "./PanelCancelButton";
 
 export default function Panel(props) {
   if (!props.show) {
     return null;
   }
-  return createPortal(
-    <>
-      <dialog
-        className={clsx(props.className, "panel")}
-        id="slice"
-        aria-labelledby="panel-heading"
-        aria-modal
-        open
-      >
-        {/* {props.hide && (
-          <div className="panel-cancel-button">
-            <PanelCancelButton hide={props.hide} />
-          </div>
-        )} */}
-
-        <div className="panel-header">
-          <h1
-            id="panel-heading"
-            className={clsx(props.headingClassName, "panel__heading")}
-          >
-            {props.heading}
-          </h1>
+  return (
+    <div
+      className={clsx(props.className, "panel")}
+      id="slice"
+      aria-labelledby="panel-heading"
+      aria-modal
+      open
+    >
+      {props.hide && (
+        <div className="panel-cancel-button">
+          <PanelCancelButton hide={props.hide} />
         </div>
-        <div className="panel-content">{props.children}</div>
-      </dialog>
-      <div className="panel-overlay"></div>
-    </>,
-    document.body
+      )}
+
+      <div className="panel-header">
+        <h1
+          id="panel-heading"
+          className={clsx(props.headingClassName, "panel__heading")}
+        >
+          {props.heading}
+        </h1>
+      </div>
+      <div className="panel-content">{props.children}</div>
+    </div>
   );
 }
 
@@ -44,32 +41,9 @@ Panel.propTypes = {
   heading: PropTypes.string.isRequired,
   headingClassName: PropTypes.string,
   className: PropTypes.string,
-  //   splashpageForm: PropTypes.bool,
+  splashpageForm: PropTypes.bool,
 };
 
 Panel.defaultProps = {
   show: false,
 };
-
-{
-  /* <div class="panel" >
-      <h1>Pure CSS panels</h1> 
-  </div>
-  
-  
-  <div class="panel" id="slice">
-      <div class="panel__content">
-          <a href="#home">
-              Close me.
-          </a>
-      </div>
-  </div>
-  
-
-  
-  <!-- Navigation -->
-  <div class="menu">
-    <a class="menu__link" href="#slice" data-hover="Slice">Slice</a>
-    <a class="menu__link" href="#fade" data-hover="Fade">Fade</a> 
-  </div> */
-}
