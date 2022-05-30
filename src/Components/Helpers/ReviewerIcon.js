3716c7a57f2aae75f67624690f2849e26a74e293
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-05-30 18:13:30 -0500

    added reviewer icon for requested reviewers list

---
+++ src/Components/Helpers/ReviewerIcon.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import randomElement from "../../Helpers/array/randomElement";
+import "./ReviewerIcon.css";
+
+const BACKGROUND_COLORS = ["#DF0B92", "#097FAA", "#AD6200", "#084391"];
+const randomBackgroundColor = (string) => {
+  const seed = string
+    .split("")
+    .reduce((seed, char) => seed + char.charCodeAt(0), 0);
+  const backgroundColor = randomElement(BACKGROUND_COLORS, seed);
+
+  return backgroundColor;
+};
+
+export default function ReviewerIcon(props) {
+  const { firstName, lastName } = props;
+  const backgroundColor = randomBackgroundColor(firstName + lastName);
+
+  return (
+    <div
+      className="reviewer-icon"
+      aria-hidden="true"
+      style={{ backgroundColor }}
+    >
+      {firstName[0] + (lastName?.[0] || "")}
+    </div>
+  );
+}
+
+ReviewerIcon.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string,
+};
