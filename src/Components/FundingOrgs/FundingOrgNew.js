d568493c6058e768cd3278b7fe1ae9c2f55a74eb
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-26 18:33:08 -0500

    added uuid to funding orgs new

--- src/Components/FundingOrgs/FundingOrgNew.js
+++ src/Components/FundingOrgs/FundingOrgNew.js
@@ -1,27 +1,46 @@
 import React from "react";
+import { useMutation } from "react-query";
 import Modal from "../design/Modal/Modal";
 import { useCurrentOrganization } from "../../Contexts/currentOrganizationContext";
-import { createFundingOrg } from "../../Services/Organizations/FundingOrgsService";
+import * as FundingOrgsService from "../../Services/Organizations/FundingOrgsService";
 import FundingOrgForm from "./FundingOrgForm";
 import "./FundingOrgNew.css";
 
 export default function FundingOrgNew(props) {
-  const { currentOrganization, organizationClient } = useCurrentOrganization();
+  const { organizationClient } = useCurrentOrganization();
 
-  const handleSubmit = (fundingOrgFields) => {
-    createFundingOrg(organizationClient, {
-      ...fundingOrgFields,
-      organizationId: currentOrganization.id,
-    })
-      .then((fundingOrg) => {
-        if (fundingOrg.id) {
-          props.onClose(fundingOrg.id);
-        }
-      })
-      .catch((error) => {
-        console.error("funding org creation error", error);
-      });
-  };
+  // const handleSubmit = (fundingOrgFields) => {
+  //   createFundingOrg(organizationClient, {
+  //     ...fundingOrgFields,
+  //     organizationId: currentOrganization.id,
+  //   })
+  //     .then((fundingOrg) => {
+  //       if (fundingOrg.id) {
+  //         props.onClose(fundingOrg.id);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("funding org creation error", error);
+  //     });
+  // };
+
+  const { mutate: createFundingOrg } = useMutation(
+    (fundingOrgFields) =>
+      FundingOrgsService.createFundingOrg(organizationClient, fundingOrgFields),
+    {
+      onSuccess: () => {
+        alert("Funding Org created!");
+        props.onClose();
+      },
+    }
+  );
+
+  function handleCreateFundingOrg(newFundingOrgFields) {
+    createFundingOrg({
+      name: newFundingOrgFields.name,
+      website: newFundingOrgFields.website,
+    });
+  }
 
   const handleCancel = () => {
     props.onClose();
@@ -33,7 +52,10 @@ export default function FundingOrgNew(props) {
       heading="Add New Funding Org"
       className="fundingorg-new"
     >
-      <FundingOrgForm onSubmit={handleSubmit} onCancel={handleCancel} />
+      <FundingOrgForm
+        onSubmit={handleCreateFundingOrg}
+        onCancel={handleCancel}
+      />
     </Modal>
   );
 }
