e49333145c36b2cf8f057e347341b688c7fe5143
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-08 21:38:48 -0500

    Replace jest with vitest

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 3001,
   },
-  build: {
-    outDir: "build",
+  plugins: [react(), svgrPlugin()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/utils/testSupport/testSetup.js",
+    api: {
+      host: "0.0.0.0",
+    },
   },
-  plugins: [reactRefresh(), svgrPlugin()],
 });
