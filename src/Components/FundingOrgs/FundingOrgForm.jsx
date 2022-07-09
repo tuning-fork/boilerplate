20a54e6696fdd09fe537f89591440671ee6a65b0
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-08 20:50:33 -0500

    Fix missing React imports

--- src/Components/FundingOrgs/FundingOrgForm.jsx
+++ src/Components/FundingOrgs/FundingOrgForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "../design/Button/Button";
 import TextBox from "../design/TextBox/TextBox";
 import "./FundingOrgForm.css";
