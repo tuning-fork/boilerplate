8f5b54f26f9fb8c7bacd13febf5eb15de6727f49
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-10 20:15:59 -0500

    Replace runtime env with compile-time env

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import runtimeEnv from "@mars/heroku-js-runtime-env";
 
-const env = runtimeEnv();
-const BASE_URL = env.VITE_API_URL || "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const apiClient = axios.create({
   baseURL: `${BASE_URL}/api/`,
