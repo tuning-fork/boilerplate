ebc64a88d35e07d8c3daa41cd74d415e90f6d1dc
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-17 17:07:40 -0500

    Proxy API requests through NGINX server
    This was done to get around having the API_URL change at
    runtime (as opposed to build time).

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-
 const apiClient = axios.create({
-  baseURL: `${BASE_URL}/api/`,
+  baseURL: "/api/",
 });
 
 export default apiClient;
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3001,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:3000",
+      },
+    },
   },
   plugins: [react(), svgrPlugin(), eslint({ failOnError: false })],
   test: {
