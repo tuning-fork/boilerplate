3eaecf20ac5a7255ffbd46cd602aaf795b47e587
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-07-21 10:00:21 -0500

    added styling for bio cards

--- src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioFrame/BioFrame.js
+++ src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioFrame/BioFrame.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { TEAM_BIOS } from "../TeamPage/TeamBios";
-import BioCard from "../BioCard/BioCard";
+// import BioCard from "../BioCard/BioCard";
 import "./BioFrame.css";
-import Frame from "../../../../design/Frame/Frame";
+// import Frame from "../../../../design/Frame/Frame";
+import mikeHeadshot from "../BioCard/mikephoto1.jpg";
+import { MdCode } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
+import { RiProfileLine, RiGithubFill } from "react-icons/ri";
 
 export default function BioFrame() {
   return (
@@ -11,19 +15,59 @@ export default function BioFrame() {
         return (
           <>
             <div className="bio-frame__card">
-              <div>
+              <div className={"bio-frame__headshot"}>
+                <img
+                  src={mikeHeadshot}
+                  alt="Mike McFaddin Headshot"
+                  className="bio-frame__headshot-image"
+                />
+              </div>
+              <h3>
                 {bio.first_name} {bio.last_name}
+              </h3>
+              <h5>{bio.title}</h5>
+              <div className={"bio-frame__icon-links"}>
+                <a
+                  href="https://michael-mcfaddin.github.io/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <MdCode />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/michael-mcfaddin/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <BsLinkedin />
+                </a>
+                <a
+                  href="https://www.google.com"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <RiProfileLine />
+                </a>
+                <a
+                  href="https://github.com/Michael-McFaddin"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <RiGithubFill />
+                </a>
               </div>
             </div>
-            ;
           </>
         );
       })}
-
-      {/* <BioCard /> */}
     </div>
   );
 }
+
+//thumbnail gallery view contains:
+//first name and last name
+//title
+//headshot
 
 //needs to contain:
 //frame element - background pixelly image
--- src/Components/Layouts/SplashpageLayout/SplashpageTeam/TeamPage/TeamPage.js
+++ src/Components/Layouts/SplashpageLayout/SplashpageTeam/TeamPage/TeamPage.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import BioFrame from "../BioFrame/BioFrame.js";
+import "./TeamPage.css";
 
 export default function TeamPage() {
-  return <BioFrame />;
+  return (
+    <div>
+      <h1 className="team-page__header">Our Team</h1>
+      <div className="team-page__content">
+        <BioFrame />
+      </div>
+    </div>
+  );
 }
 //gallery of mini bio cards with headshot, name, and title
 //...click on one
