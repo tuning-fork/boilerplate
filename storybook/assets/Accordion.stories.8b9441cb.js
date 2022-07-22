e29c15d2c2481030ff9931ebec3ed1bef4b65dc5
Jess-White <Jess-White@users.noreply.github.com>
2022-07-22 19:37:57 +0000

    deploy: 442b683ea7f6775704e1d87dcd7efacb025fffd3

---
+++ storybook/assets/Accordion.stories.8b9441cb.js
@@ -0,0 +1,45 @@
+import{a as e,j as o}from"./jsx-runtime.7911e018.js";import{A as r}from"./Accordion.783fddb1.js";import{A as n,a as i,b as t}from"./AccordionItemPanel.bfe41e8a.js";import"./iframe.cb30e208.js";import"./index.9ab846ab.js";import"./clsx.m.c5ef2623.js";import"./module.fd773be3.js";import"./index.esm.24f1c133.js";import"./iconBase.1024b5bb.js";import"./Button.f5e66bdb.js";const b={parameters:{storySource:{source:`import React from "react";
+import Component, {
+  AccordionItem,
+  AccordionItemHeader,
+  AccordionItemPanel,
+} from "./Accordion";
+
+export default {
+  title: "Design/Accordion",
+  component: Component,
+};
+
+export const Accordion = (props) => (
+  <Component {...props}>
+    <AccordionItem>
+      <AccordionItemHeader heading="h2">Mission Statement</AccordionItemHeader>
+      <AccordionItemPanel>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </AccordionItemPanel>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionItemHeader heading="h2">About Our Team</AccordionItemHeader>
+      <AccordionItemPanel>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </AccordionItemPanel>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionItemHeader heading="h2">Budget</AccordionItemHeader>
+      <AccordionItemPanel>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </AccordionItemPanel>
+    </AccordionItem>
+  </Component>
+);
+;Accordion.__docgenInfo={"description":"","methods":[],"displayName":"Accordion"}`,locationsMap:{accordion:{startLoc:{col:25,line:13},endLoc:{col:1,line:43},startBody:{col:25,line:13},endBody:{col:1,line:43}}}}},title:"Design/Accordion",component:r},d=c=>e(r,{...c,children:[e(n,{children:[o(i,{heading:"h2",children:"Mission Statement"}),o(t,{children:o("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})})]}),e(n,{children:[o(i,{heading:"h2",children:"About Our Team"}),o(t,{children:o("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})})]}),e(n,{children:[o(i,{heading:"h2",children:"Budget"}),o(t,{children:o("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})})]})]});d.__docgenInfo={description:"",methods:[],displayName:"Accordion"};const f=["Accordion"];export{d as Accordion,f as __namedExportsOrder,b as default};
+//# sourceMappingURL=Accordion.stories.8b9441cb.js.map
