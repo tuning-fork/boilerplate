d278df47d94feb94bcea54871606abdddd7446ee
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-23 18:10:12 -0500

    Extract organization cards out into separate component and add avatar stack

--- src/Components/Organizations/OrganizationIndex.jsx
+++ src/Components/Organizations/OrganizationIndex.jsx
@@ -3,69 +3,11 @@ import { Link } from "react-router-dom";
 import { useCurrentOrganization } from "../../Contexts/currentOrganizationContext";
 import Container from "../design/Container/Container";
 import Button from "../design/Button/Button";
+import OrganizationCard from "./OrganizationIndex/OrganizationCard";
 import "./OrganizationIndex.css";
 
 export default function OrganizationIndex() {
-  // const { organizations } = useCurrentOrganization();
-
-  const organizations = [
-    {
-      id: "1",
-      name: "Mindy St. Claire Foundation for the Ethics-Impaired",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "2",
-      name: "Al-Jamil Foundation",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "3",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "4",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "5",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "6",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "7",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "8",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "9",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "10",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-    {
-      id: "11",
-      name: "Save the Shrimp Fund",
-      users: [{ id: "1", firstName: "Chidi", lastName: "Anagonye" }],
-    },
-  ];
-  console.log({ organizations });
+  const { organizations } = useCurrentOrganization();
 
   return (
     <Container as="section" className="organization-index">
@@ -79,17 +21,7 @@ export default function OrganizationIndex() {
         {organizations.map((organization) => (
           <li key={organization.id} className="organization-index__list-item">
             <Link to={`/organizations/${organization.id}/dashboard`}>
-              <article className="organization-index__organization-card">
-                <h2>{organization.name}</h2>
-                <ul className="organization-index__user-list">
-                  {organization.users &&
-                    organization.users.map((user) => (
-                      <li>
-                        {user.firstName} {user.lastName}
-                      </li>
-                    ))}
-                </ul>
-              </article>
+              <OrganizationCard organization={organization} />
             </Link>
           </li>
         ))}
