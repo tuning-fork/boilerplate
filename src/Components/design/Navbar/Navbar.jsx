91106d8472eaa916b69845e5595f7aef145d6b49
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-24 15:28:04 -0500

    Minor clean up in NavBar component

--- src/Components/design/Navbar/Navbar.jsx
+++ src/Components/design/Navbar/Navbar.jsx
@@ -18,11 +18,8 @@ export default function Navbar(props) {
         logout();
         history.replace("/splashpage", { loggedOut: true });
         break;
-      // case "USER_MENU":
-      //   console.log("waffle!");
-      //   break;
       default:
-        console.log("default");
+        throw new TypeError("Unexpected option given to dropdown");
     }
   };
 
@@ -43,10 +40,7 @@ export default function Navbar(props) {
           <DropdownMini
             className="navbar__see-more"
             dropDownMenuClassName="navbar__dropdown-menu"
-            options={[
-              { value: "LOGOUT", label: "Logout" },
-              // { value: "USER_MENU", label: "User Menu" },
-            ]}
+            options={[{ value: "LOGOUT", label: "Logout" }]}
             displayIcon={
               <Avatar>
                 {props.user.firstName} {props.user.lastName}
@@ -62,6 +56,9 @@ export default function Navbar(props) {
 
 Navbar.propTypes = {
   className: PropTypes.string,
-  user: PropTypes.object.isRequired,
+  user: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string,
+  }).isRequired,
   organizationName: PropTypes.string,
 };
