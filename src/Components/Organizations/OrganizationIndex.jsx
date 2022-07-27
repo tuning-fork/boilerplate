b93d50d799b387d18171be841315766aaf4c5297
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-07-26 23:44:10 -0500

    Add flames background to OrganizationIndeex

--- src/Components/Organizations/OrganizationIndex.jsx
+++ src/Components/Organizations/OrganizationIndex.jsx
@@ -8,6 +8,7 @@ import OrganizationForm from "./OrganizationForm";
 import Container from "../design/Container/Container";
 import Button from "../design/Button/Button";
 import Modal from "../design/Modal/Modal";
+import Background from "../design/Background/Background";
 import { withNavbarLayout } from "../Layouts/NavbarLayout/NavbarLayout";
 import "./OrganizationIndex.css";
 
@@ -32,32 +33,34 @@ function OrganizationIndex() {
   );
 
   return (
-    <Container as="section" className="organization-index">
-      <header className="organization-index__header">
-        <h1>Organizations</h1>
-        <Button
-          className="organization-index__new-button"
-          onClick={() => setIsNewOrganizationModalOpen(true)}
-        >
-          Add New Organization
-        </Button>
-      </header>
-      <ul className="organization-index__list">
-        {organizations.map((organization) => (
-          <li key={organization.id} className="organization-index__list-item">
-            <Link to={`/organizations/${organization.id}/dashboard`}>
-              <OrganizationCard organization={organization} />
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Modal show={isNewOrganizationModalOpen} heading="Add Organization">
-        <OrganizationForm
-          onSubmit={createOrganization}
-          onCancel={() => setIsNewOrganizationModalOpen(false)}
-        />
-      </Modal>
-    </Container>
+    <Background>
+      <Container as="section" className="organization-index">
+        <header className="organization-index__header">
+          <h1>Organizations</h1>
+          <Button
+            className="organization-index__new-button"
+            onClick={() => setIsNewOrganizationModalOpen(true)}
+          >
+            Add New Organization
+          </Button>
+        </header>
+        <ul className="organization-index__list">
+          {organizations.map((organization) => (
+            <li key={organization.id} className="organization-index__list-item">
+              <Link to={`/organizations/${organization.id}/dashboard`}>
+                <OrganizationCard organization={organization} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Modal show={isNewOrganizationModalOpen} heading="Add Organization">
+          <OrganizationForm
+            onSubmit={createOrganization}
+            onCancel={() => setIsNewOrganizationModalOpen(false)}
+          />
+        </Modal>
+      </Container>
+    </Background>
   );
 }
 
