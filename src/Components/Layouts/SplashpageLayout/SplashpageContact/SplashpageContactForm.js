ffc9fd6aa3bf2d1201c41c9c79ec1aa96b5927e0
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-08-04 18:15:22 -0500

    fixed linting error unused import

--- src/Components/Layouts/SplashpageLayout/SplashpageContact/SplashpageContactForm.js
+++ src/Components/Layouts/SplashpageLayout/SplashpageContact/SplashpageContactForm.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Button from "../../../design/Button/Button";
 import Container from "../../../design/Container/Container";
