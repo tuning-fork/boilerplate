356840565f2c260803b9fa773f699e94de1f08c9
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-08-11 09:00:16 -0500

    added styling for features page

--- src/Components/Layouts/SplashpageLayout/SplashpageFeatures/FeatureCard.jsx
+++ src/Components/Layouts/SplashpageLayout/SplashpageFeatures/FeatureCard.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./FeatureCard.css";
-import Sample_screenshot from "Sample_screenshot.png";
+import Sample_screenshot from "./Sample_screenshot.png";
 
-export default function FeatureCard(props) {
+export default function FeatureCard({ feature }) {
   return (
     <>
       <div className={"feature-card__container"}>
         <div>
-          <h1 className="feature-card__name">{props.currentFeature.name}</h1>
-        </div>
-        <div className={"feature-card__content"}>
-          <div className={"feature-card__text-frame"}>
-            <div className={"feature-card__text"}>{FEATURE_TEXT}</div>
-          </div>
-          <div className={"feature-card__screenshot-frame"}>
+          <h2 className="feature-card__name">{feature.name}</h2>
+          <div>
             <img
               src={Sample_screenshot}
               alt="Feature Screenshot Image"
               className="feature-card__screenshot-image"
             />
           </div>
+        </div>
+        <div className={"feature-card__text-frame"}>
+          <div className={"feature-card__text"}>{feature.text}</div>
         </div>
       </div>
     </>
