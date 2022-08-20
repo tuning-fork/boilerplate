b9b067c0b3ea8cad47853c02a59ab0a7eada6345
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-08-20 14:50:03 -0500

    Add documentation for useQuery hook

--- src/Hooks/useQuery.js
+++ src/Hooks/useQuery.js
@@ -1,6 +1,18 @@
 import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
 
+/**
+ * @example
+ *
+ * // url = 'http://localhost:3001?token=abc&email=
+ * // token = abc
+ * // email = ''
+ * // something = null
+ * const query = useQuery()
+ * const token = useMemo(() => query.get('token'), [query])
+ * const email = useMemo(() => query.get('email'), [query])
+ * const something = useMemo(() => query.get('something'), [query])
+ */
 export default function useQuery() {
   const { search } = useLocation();
 
