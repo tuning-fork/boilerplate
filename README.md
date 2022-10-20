b599b24760c76fda45699b4cd0b9a208cd203ce8
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-10-19 19:36:11 -0500

    added invitation tabs to user index page

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ applications.
 
 For instructions on contributing, check out
 [CONTRIBUTING.md](./docs/CONTRIBUTING.md).
+
+~~0. delete these two actions: organization_users#create and organization_users#show~~
+1. add a new tab for the user page that shows invitations
+  * fetch invitations from api through a new invitations service
+2. create a popover reuseable component
+  * used for when you hover over the buttons and icons
+3. implement revinvite icon-button in invitations table
+4. implement unvinvite icon-button in invitations table
+5. implement remove user icon-button in user table
+6. implement invite user button 
+  * only available to admins
+  * create some modal with the form fields
+7. implement icons in invitations table
+  * expired
+  * pending
+8. follow up on giving frontend ability to know if current user is an admin and
+   hiding users link from sidebar if not an admin
+8. follow up on restricting organization user methods on backend (index)
--- src/pages/UserIndex/UserIndexPage.jsx
+++ src/pages/UserIndex/UserIndexPage.jsx
@@ -1,25 +1,38 @@
 import React, { useState, useMemo, useContext } from "react";
 import { useQuery } from "react-query";
+import clsx from "clsx";
 import * as OrganizationService from "../../Services/OrganizationService";
 import { CurrentOrganizationContext } from "../../Contexts/currentOrganizationContext";
+import Button from "../../Components/design/Button/Button";
 import Table from "../../Components/design/Table/Table";
 import TextBox from "../../Components/design/TextBox/TextBox";
 import "./UserIndexPage.css";
 
+const Tabs = {
+  USERS: "USERS",
+  INVITATIONS: "INVITATIONS",
+};
+
 export default function UserIndexPage() {
+  const [tabSelect, setTabSelect] = useState(Tabs.USERS);
   const { organizationClient } = useContext(CurrentOrganizationContext);
   const [searchString, setSearchString] = useState("");
   const { data: users } = useQuery("getUsers", () =>
     OrganizationService.getAllOrganizationUsers(organizationClient)
   );
-  const filteredUsers = useMemo(() => {
-    return users.filter((user) => {
-      const userFullName = `${user.firstName} ${user.lastName}`;
-      return userFullName
-        .toLowerCase()
-        .includes(searchString.toLowerCase().trim());
-    });
-  }, [users, searchString]);
+  const filteredResources = useMemo(() => {
+    if (tabSelect === Tabs.USERS) {
+      return users.filter((user) => {
+        const userFullName = `${user.firstName} ${user.lastName}`;
+        return userFullName
+          .toLowerCase()
+          .includes(searchString.toLowerCase().trim());
+      });
+    } else if (tabSelect === Tabs.INVITATIONS) {
+      // todo
+      return [];
+    }
+  }, [tabSelect, users, searchString]);
 
   const columns = [
     { Header: "First Name", accessor: "firstName" },
@@ -38,11 +51,34 @@ export default function UserIndexPage() {
           className="user-index__search-input"
         />
       </div>
-      {filteredUsers.length ? (
+      <div className="user-index__table-tabs">
+        <Button
+          onClick={() => setTabSelect(Tabs.USERS)}
+          className={clsx(
+            "user-index__table-tab-button",
+            tabSelect === Tabs.USERS && "user-index__table-tab-button--selected"
+          )}
+          variant="text"
+        >
+          Users
+        </Button>
+        <Button
+          onClick={() => setTabSelect(Tabs.INVITATIONS)}
+          className={clsx(
+            "user-index__table-tab-button",
+            tabSelect === Tabs.INVITATIONS &&
+              "user-index__table-tab-button--selected"
+          )}
+          variant="text"
+        >
+          Invitations
+        </Button>
+      </div>
+      {filteredResources.length ? (
         <Table
           className="user-index__table"
           columns={columns}
-          data={filteredUsers}
+          data={filteredResources}
         />
       ) : (
         <p>No users found.</p>
