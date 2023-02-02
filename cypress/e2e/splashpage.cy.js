5d7f5ea9827c408f3b3653ab9c9713d5805ef5b8
Michael-McFaddin <52680887+Michael-McFaddin@users.noreply.github.com>
2023-02-02 10:03:01 -0500

    wip to drag and drop

--- cypress/e2e/splashpage.cy.js
+++ cypress/e2e/splashpage.cy.js
@@ -3,20 +3,42 @@ describe("Splashpage", () => {
     cy.viewport(2560, 1600);
     cy.visit("http://localhost:3001");
     cy.get('[data-testid="log-in-button"]').click();
-    cy.get("input[type=email]").type("michael@thegoodplace.com", {
+    cy.get("input[type=email]").type(
+      "michael@thegoodplace.com" /* {
       delay: 100,
-    });
-    cy.get("input[type=password]").type("password", {
-      delay: 100,
-    });
+    } */
+    );
+    cy.get("input[type=password]").type("password");
     cy.get("button[type=submit]").click();
     cy.get('[data-testid="The Good Place"]').click();
     cy.get('[data-testid="Grants"]').click();
     cy.get('[data-testid="Good Place Neighborhood Grant"]').click();
     cy.get('[data-testid="overview"]').click();
-    cy.get('[data-testid="Organization Overview"]')
-      .trigger("mousedown", { which: 0 })
-      .trigger("mousemove", { clientX: 0, clientY: 100 })
-      .trigger("mouseup", { force: true });
+    cy.get('[data-testid="Programs"]');
+    // cy.get(target)
+    //     .then($target => {
+    //         let coords = $target[0].getBoundingClientRect();
+    // });
+    cy.get('[data-testid="Programs"]')
+      .then((res) => {
+        return res[0].getBoundingClientRect();
+      })
+      .then((res) => {
+        cy.get('[data-testid="Organization Overview"]')
+          .trigger("mousedown", { force: true })
+          .wait(2000)
+          .trigger("mousemove", { clientX: res.x, clientY: res.y, force: true })
+          .wait(2000)
+          .trigger("mouseup", { force: true });
+      });
+
+    // cy.get('[data-testid="Organization Overview"]')
+    //   .trigger("mousedown", "topLeft")
+    //   // .wait(2000)
+    //   .trigger("mousemove", {
+    //     // force: true,
+    //     clientX: coords[0],
+    //     clientY: coords[1],
+    //   });
   });
 });
