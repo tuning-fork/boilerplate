869b3d132e17a059bf9e6b794e197c1969fc1af0
Michael-McFaddin <52680887+Michael-McFaddin@users.noreply.github.com>
2023-02-07 10:00:01 -0500

    WIP 02-07-23

--- cypress/e2e/splashpage.cy.js
+++ cypress/e2e/splashpage.cy.js
@@ -1,5 +1,6 @@
 describe("Splashpage", () => {
   it("Logs into the application", () => {
+    // #1 --------------------------------------------------------------------------
     // cy.viewport(2560, 1600);
     // cy.visit("http://localhost:3001");
     // cy.get('[data-testid="log-in-button"]').click();
@@ -14,39 +15,38 @@ describe("Splashpage", () => {
     // cy.get('[data-testid="Grants"]').click();
     // cy.get('[data-testid="Good Place Neighborhood Grant"]').click();
     // cy.get('[data-testid="overview"]').click();
+    // const dataTransfer = new DataTransfer();
+    // cy.get('[data-testid="Organization Overview"]').trigger("dragstart", {
+    //   dataTransfer,
+    // });
+    // cy.get('[data-testid="Programs"]').trigger("drop", { dataTransfer });
+    // #2 --------------------------------------------------------------------------
     // cy.get('[data-testid="overview"]').should("exist");
     // cy.get('[data-testid="Programs"]').should("exist");
-
     // // Cypress.Commands.add("set_typeCarName", () => {
     // //   return cy.wrap(webnewsite.set_typeCarName()); //return the wrap and use in chain
     // // });
-
     // // function set_typeCarName() {
     // //   let carName = "CAR 1";
     // //   cy.get("#newSiteCityInput").type(carName);
     // //   cy.log("log1 = " + carName);
     // //   return carName;
     // // }
-
     // function set_draggable() {
     //   cy.get('[data-testid="Organizations Overview"]');
     // }
-
     // function set_droppable() {
     //   cy.get('[data-testid="Programs"]');
     // }
-
     // Cypress.Commands.add("set_draggable", () => {
     //   return cy.wrap(set_draggable()); //return the wrap and use in chain
     // });
-
     // Cypress.Commands.add("set_droppable", () => {
     //   return cy.wrap(set_droppable()); //return the wrap and use in chain
     // });
     // // const droppable = cy.get('[data-testid="Programs"]')[0]; // Drop over this
     // // const draggable = cy.get('[data-testid="overview"]'); // Pick up this
     // // const droppable = cy.get('[data-testid="Programs"]')[0]; // Drop over this
-
     // // cy.set_droppable().getBoundingClientRect();
     // cy.set_draggable().dispatchEvent(new MouseEvent("mousedown"));
     // cy.set_draggable().dispatchEvent(
@@ -61,6 +61,130 @@ describe("Splashpage", () => {
     // );
     // cy.set_draggable().dispatchEvent(new MouseEvent("mouseup"));
     // cy.get('[data-testid="Programs"]');
+    // #3 --------------------------------------------------
+    // Cypress.Commands.add(
+    //   "loginGoToOverview",
+    //   {
+    //     prevSubject: false,
+    //   },
+    //   (email, pass) => {
+    //     cy.viewport(2560, 1600);
+    //     cy.visit("http://localhost:3001");
+    //     cy.get('[data-testid="log-in-button"]').click();
+    //     cy.get("input[type=email]").type(
+    //       email /* {
+    //   delay: 100,
+    // } */
+    //     );
+    //     cy.get("input[type=password]").type(pass);
+    //     cy.get("button[type=submit]").click();
+    //     cy.get('[data-testid="The Good Place"]').click();
+    //     cy.get('[data-testid="Grants"]').click();
+    //     cy.get('[data-testid="Good Place Neighborhood Grant"]').click();
+    //     cy.get('[data-testid="overview"]').click();
+    //   }
+    // );
+    // Cypress.Commands.add("dragAndDrop", (subject, target) => {
+    //   Cypress.log({
+    //     name: "DRAGNDROP",
+    //     message: `Dragging element ${subject} to ${target}`,
+    //     consoleProps: () => {
+    //       return {
+    //         subject: subject,
+    //         target: target,
+    //       };
+    //     },
+    //   });
+    //   const BUTTON_INDEX = 0;
+    //   const SLOPPY_CLICK_THRESHOLD = 10;
+    //   cy.get(target)
+    //     .first()
+    //     .then(($target) => {
+    //       let coordsDrop = $target[0].getBoundingClientRect();
+    //       cy.get(subject)
+    //         .first()
+    //         .then((subject) => {
+    //           const coordsDrag = subject[0].getBoundingClientRect();
+    //           cy.wrap(subject)
+    //             .trigger("mousedown", {
+    //               button: BUTTON_INDEX,
+    //               clientX: coordsDrag.x,
+    //               clientY: coordsDrag.y,
+    //               force: true,
+    //             })
+    //             .trigger("mousemove", {
+    //               button: BUTTON_INDEX,
+    //               clientX: coordsDrag.x + SLOPPY_CLICK_THRESHOLD,
+    //               clientY: coordsDrag.y,
+    //               force: true,
+    //             });
+    //           cy.get("body")
+    //             .trigger("mousemove", {
+    //               button: BUTTON_INDEX,
+    //               clientX: coordsDrop.x,
+    //               clientY: coordsDrop.y,
+    //               force: true,
+    //             })
+    //             .trigger("mouseup");
+    //         });
+    //     });
+    // });
+    // cy.loginGoToOverview("michael@thegoodplace.com", "password");
+    // cy.dragAndDrop(
+    //   '[data-testid="Organization Overview"]',
+    //   '[data-testid="Programs"]'
+    // );
+    //--------------------------------------------------
+    // #4----------------------------------------------------
+    // const dataTransfer = new DataTransfer();
+    // cy.viewport(2560, 1600);
+    // cy.visit("http://localhost:3001");
+    // cy.get('[data-testid="log-in-button"]').click();
+    // cy.get("input[type=email]").type(
+    //   "michael@thegoodplace.com" /* {
+    //   delay: 100,
+    // } */
+    // );
+    // cy.get("input[type=password]").type("password");
+    // cy.get("button[type=submit]").click();
+    // cy.get('[data-testid="The Good Place"]').click();
+    // cy.get('[data-testid="Grants"]').click();
+    // cy.get('[data-testid="Good Place Neighborhood Grant"]').click();
+    // cy.get('[data-testid="overview"]').click();
+    // let coordsDrop = { x: null, y: null };
+    // cy.get('[data-testid="Organization Overview"]').trigger("dragstart", {
+    //   dataTransfer,
+    // });
+    // cy.get('[data-testid="Programs"]')
+    //   .then(($target) => {
+    //     coordsDrop = $target[0].getBoundingClientRect();
+    //   })
+    //   .trigger("dragenter", {
+    //     // button: BUTTON_INDEX,
+    //     clientX: coordsDrop.x,
+    //     clientY: coordsDrop.y,
+    //     force: true,
+    //     dataTransfer,
+    //   })
+    //   .trigger("dragover", {
+    //     // button: BUTTON_INDEX,
+    //     clientX: coordsDrop.x,
+    //     clientY: coordsDrop.y,
+    //     force: true,
+    //     dataTransfer,
+    //   })
+    //   .trigger("drop", {
+    //     // button: BUTTON_INDEX,
+    //     clientX: coordsDrop.x,
+    //     clientY: coordsDrop.y,
+    //     force: true,
+    //     dataTransfer,
+    //   })
+    //   .wait(50)
+    //   .trigger("dragend", { force: true });
+    // ------------------------------------------------------
+    // #5 --------------------------------------------------
+    const dataTransfer = new DataTransfer();
     Cypress.Commands.add(
       "loginGoToOverview",
       {
@@ -83,7 +207,6 @@ describe("Splashpage", () => {
         cy.get('[data-testid="overview"]').click();
       }
     );
-
     Cypress.Commands.add("dragAndDrop", (subject, target) => {
       Cypress.log({
         name: "DRAGNDROP",
@@ -106,36 +229,45 @@ describe("Splashpage", () => {
             .then((subject) => {
               const coordsDrag = subject[0].getBoundingClientRect();
               cy.wrap(subject)
-                .trigger("mousedown", {
+                .trigger("dragstart", {
                   button: BUTTON_INDEX,
                   clientX: coordsDrag.x,
                   clientY: coordsDrag.y,
                   force: true,
+                  dataTransfer,
                 })
-                .trigger("mousemove", {
+                .trigger("dragenter", {
                   button: BUTTON_INDEX,
-                  clientX: coordsDrag.x + SLOPPY_CLICK_THRESHOLD,
-                  clientY: coordsDrag.y,
+                  clientX: coordsDrop.x + SLOPPY_CLICK_THRESHOLD,
+                  clientY: coordsDrop.y,
                   force: true,
+                  dataTransfer,
+                })
+                .trigger("dragover", {
+                  button: BUTTON_INDEX,
+                  clientX: coordsDrop.x + SLOPPY_CLICK_THRESHOLD,
+                  clientY: coordsDrop.y,
+                  force: true,
+                  dataTransfer,
                 });
               cy.get("body")
-                .trigger("mousemove", {
+                .trigger("drop", {
                   button: BUTTON_INDEX,
                   clientX: coordsDrop.x,
                   clientY: coordsDrop.y,
                   force: true,
+                  dataTransfer,
                 })
-                .trigger("mouseup");
+                .trigger("dragend");
             });
         });
     });
-
     cy.loginGoToOverview("michael@thegoodplace.com", "password");
     cy.dragAndDrop(
       '[data-testid="Organization Overview"]',
       '[data-testid="Programs"]'
     );
-
+    //--------------------------------------------------
     // // cy.viewport(2560, 1600);
     // // cy.visit("http://localhost:3001");
     // // cy.get('[data-testid="log-in-button"]').click();
@@ -155,7 +287,6 @@ describe("Splashpage", () => {
     // //     .then($target => {
     // //         let coords = $target[0].getBoundingClientRect();
     // // });
-
     // Cypress.Commands.add(
     //   "mouseMoveBy",
     //   {
@@ -167,7 +298,6 @@ describe("Splashpage", () => {
     //         console.log("subject", subject);
     //         const initialRect = subject.get(0).getBoundingClientRect();
     //         // const windowScroll = getDocumentScroll();
-
     //         return [subject, initialRect /* , windowScroll */];
     //       })
     //       .then(([subject, initialRect]) => {
@@ -202,7 +332,6 @@ describe("Splashpage", () => {
     //             //   x: windowScroll.x - initialWindowScroll.x,
     //             //   y: windowScroll.y - initialWindowScroll.y,
     //             // };
-
     //             // const delta = {
     //             //   x: Math.round(
     //             //     finalRect.left - initialRect.left - windowScrollDelta.x
@@ -211,20 +340,17 @@ describe("Splashpage", () => {
     //             //     finalRect.top - initialRect.top - windowScrollDelta.y
     //             //   ),
     //             // };
-
     //             return { subject, initialRect, finalRect };
     //           });
     //       });
     //   }
     // );
-
     // Cypress.Commands.addAll({
     //   loginGoToOverview(e, p) {},
     //   mouseMoveBy(a) {},
     // });
     // cy.loginGoToOverview("michael@thegoodplace.com", "password");
     // cy.mouseMoveBy(`[data-testid="Programs"]`);
-
     // cy.get('[data-testid="Programs"]')
     //   .then((res) => {
     //     return res[0].getBoundingClientRect();
@@ -243,7 +369,6 @@ describe("Splashpage", () => {
     //       // .wait(2000)
     //       .trigger("mouseup", { force: true });
     //   });
-
     // cy.get('[data-testid="Organization Overview"]')
     //   .trigger("mousedown", "topLeft")
     //   // .wait(2000)
