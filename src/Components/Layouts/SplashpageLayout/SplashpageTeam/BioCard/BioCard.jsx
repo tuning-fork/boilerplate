fd456d66423f934188b2790a4498724e7389a1d5
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2023-02-16 19:52:40 -0600

    design updates in progress

--- src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioCard/BioCard.jsx
+++ src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioCard/BioCard.jsx
@@ -12,7 +12,38 @@ export default function BioCard(props) {
           <h1 className="bio-card__bio-name">
             {props.currentBio.first_name} {props.currentBio.last_name}
           </h1>
-          <h3 className="bio-card__bio-title">{props.currentBio.title}</h3>
+          <div className="bio-card__info">
+            <h3 className="bio-card__bio-title">{props.currentBio.title}</h3>
+            {/* <div className={"bio-card__icon-links"}> */}
+            {props.currentBio.personal_site && (
+              <a
+                href={props.currentBio.personal_site}
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <MdCode />
+              </a>
+            )}
+            {props.currentBio.linkedin && (
+              <a
+                href={props.currentBio.linkedin}
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <BsLinkedin />
+              </a>
+            )}
+            {props.currentBio.github && (
+              <a
+                href={props.currentBio.github}
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <RiGithubFill />
+              </a>
+            )}
+            {/* </div> */}
+          </div>
         </div>
         <div className={"bio-card__bio-content"}>
           <div className={"bio-card__text-frame"}>
@@ -20,40 +51,11 @@ export default function BioCard(props) {
               className={"bio-card__text"}
               dangerouslySetInnerHTML={{ __html: props.currentBio.bio_text }}
             ></div>
-            <div className={"bio-card__icon-links"}>
-              {props.currentBio.personal_site && (
-                <a
-                  href={props.currentBio.personal_site}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <MdCode />
-                </a>
-              )}
-              {props.currentBio.linkedin && (
-                <a
-                  href={props.currentBio.linkedin}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <BsLinkedin />
-                </a>
-              )}
-              {props.currentBio.github && (
-                <a
-                  href={props.currentBio.github}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <RiGithubFill />
-                </a>
-              )}
-            </div>
           </div>
           <div className={"bio-card__headshot-frame"}>
             <img
               src={props.currentBio.headshot_url}
-              alt="Mike McFaddin Headshot"
+              alt={props.currentBio.headshot_alt}
               className="bio-card__headshot-image"
             />
           </div>
--- src/Components/Layouts/SplashpageLayout/SplashpageTeam/TeamPage/TeamBios.jsx
+++ src/Components/Layouts/SplashpageLayout/SplashpageTeam/TeamPage/TeamBios.jsx
@@ -1,9 +1,9 @@
 export const TEAM_BIOS = [
-  // TODO: Iteratively add these in!
   {
     first_name: "Mike",
     last_name: "McFaddin",
     headshot_url: "/imgs/mike.jpg",
+    headshot_alt: "",
     title: "Software Engineer",
     github: "https://github.com/Michael-McFaddin",
     linkedin: "https://www.linkedin.com/in/michael-mcfaddin/",
@@ -28,10 +28,10 @@ export const TEAM_BIOS = [
     first_name: "Cain",
     last_name: "Watson",
     title: "Senior Software Engineer",
+    headshot_alt: "",
     // headshot_url: "/imgs/Cain.jpg",
     github: "https://github.com/cainwatson",
     linkedin: "https://www.linkedin.com/in/cainwatson/",
-    // personal_site: "https://www.google.com",
     bio_text: `
       Cain started attending Operation Spark, a New Orleans-based coding bootcamp, while in his sophomore year of high school and quickly found out he had a knack for programming. His passion for learning and teaching new ideas and technologies led him to later become a teacher for the organization! As a teacher and as an engineer, Cain strives to bring personality, empathy, and fun to the otherwise boring or tedious parts of software development. In his spare time, Cain enjoys fashion, drawing, and learning foreign languages -- he's currently learning Mandarin!
       <br>
@@ -52,10 +52,10 @@ export const TEAM_BIOS = [
     first_name: "Jess",
     last_name: "White",
     title: "Software Engineer",
+    headshot_alt: "Jess White headshot",
     // headshot_url: "/imgs/Jess.jpg",
     github: "https://github.com/jess-white",
     linkedin: "https://www.linkedin.com/in/jess-white-chicago/",
-    // personal_site: "https://www.google.com",
     bio_text: `
       Jess came up with the idea for Boilerplate while she was still in bootcamp, and has spent the past two years growing her coding skills along with the app. She believes that coding is communication, and that responsive, empathetic design is critical to providing equal access.
       <br>
@@ -72,36 +72,59 @@ export const TEAM_BIOS = [
       When she is not coding, she reads and cooks. Currently, she’s re-reading Iain M. Banks’s Culture series. She also recently made a goat cheese cheesecake (excellent) and a blue cheese cheesecake (vile).
     `,
   },
-  // {
-  //   first_name: "Taylor",
-  //   last_name: "Shepard",
-  //   title: "Software Engineer",
-  //   headshot_url: "/imgs/Taylor.jpg",
-  //   linkedin: "https://www.linkedin.com/in/michael-mcfaddin/",
-  //   personal_site: "https://www.google.com",
-  //   bio_text:
-  //     "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.”",
-  // },
-  // {
-  //   first_name: "Amy",
-  //   last_name: "Johnson",
-  //   title: "Designer (emeritus)",
-  //   headshot_url: "/imgs/Amy.jpg",
-  //   github: "https://github.com/Michael-McFaddin",
-  //   linkedin: "https://www.linkedin.com/in/michael-mcfaddin/",
-  //   personal_site: "https://www.google.com",
-  //   bio_text:
-  //     "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.”",
-  // },
-  // {
-  //   first_name: "Eli",
-  //   last_name: "Sidman",
-  //   title: "Designer (emeritus)",
-  //   headshot_url: "/imgs/Eli.jpg/",
-  //   github: "https://github.com/Michael-McFaddin",
-  //   linkedin: "https://www.linkedin.com/in/michael-mcfaddin/",
-  //   personal_site: "https://www.google.com",
-  //   bio_text:
-  //     "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.”",
-  // },
+  {
+    first_name: "Taylor",
+    last_name: "Shepard",
+    title: "Software Engineer",
+    headshot_url: "/imgs/Taylor.jpg",
+    headshot_alt: "",
+    linkedin: "https://www.linkedin.com/in/taylorshephard/",
+    bio_text: "",
+  },
+  {
+    first_name: "Amy",
+    last_name: "Johnson",
+    title: "Designer (emeritus)",
+    headshot_url: "/imgs/Amy.jpg",
+    headshot_alt: "",
+    linkedin: "https://www.linkedin.com/in/johnsonjustified/",
+    personal_site: "https://johnsonjustified.com",
+    bio_text: `Amy is a Senior Product Designer and accessibility advocate. She currently works at Sprout Social on the Engagement team where she works with product and customers alike on Sprout’s social media management tool. She formerly worked at Tandem, Arity, Allstate, NextRadio, and Motorola Mobility. 
+      <br>
+      <br>
+
+      Amy is also a Neurodiversity advocate and has given talks about her journey as a late-diagnosed Autistic Product Designer across the United States and UK.
+      <br>
+      <br>
+
+      In her spare time, Amy can be found painting, walking her two very spoiled dogs or playing in a Magic: The Gathering Tournament.`,
+  },
+  {
+    first_name: "Eli",
+    last_name: "Sidman",
+    title: "Designer (emeritus)",
+    headshot_url: "/imgs/Eli.jpg/",
+    headshot_alt: "",
+    linkedin: "https://www.linkedin.com/in/elisidman/",
+    personal_site: "https://elisidman.com/about",
+    bio_text: `
+      With a background in physics and education, Eli is driven to understand the "why" behind a problem, and to inspire that curiosity in others.
+      <br>
+      <br>
+
+      Eli stumbled upon user-centered design while creating interactive science demonstrations as a lab instructor in the MIT physics department. He became fascinated by intentionally designing learning objects and experiences, which he pursued at the School of the Art Institute of Chicago (SAIC), receiving a masters in Designed Objects.
+      <br>
+      <br>
+
+      After working in education spaces on user research and physical learning kits, he discovered that much of the most exciting work was in the digital space. He is currently Lead Product Designer at TutorMe.
+      <br>
+      <br>
+
+      In addition to teaching physics, he has taught design thinking to undergraduates at SAIC, and developed and led summer workshops for middle school students. As an educator, his goal is to draw students as far as possible into self designing their own learning experiences.
+      <br>
+      <br>
+
+      Beyond his design work, he experiments with photography and printmaking, often turning his camera to the sky to take astronomical photos.
+    `,
+  },
 ];
