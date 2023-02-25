2d6eb21b937f66d625761c00c20e54404b48f9f0
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2023-02-25 13:19:19 -0600

    Continue adjusting bio cards for team page
    Co-authored-by: jess-white-home-chef <jess-white-home-chef@users.noreply.github.com>

--- src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioCard/BioCard.jsx
+++ src/Components/Layouts/SplashpageLayout/SplashpageTeam/BioCard/BioCard.jsx
@@ -2,65 +2,63 @@ import React from "react";
 import { MdCode } from "react-icons/md";
 import { BsLinkedin } from "react-icons/bs";
 import { RiGithubFill } from "react-icons/ri";
+import { ReactComponent as Fire } from "./bigfire.svg";
 import "./BioCard.css";
 
 export default function BioCard(props) {
   return (
-    <>
-      <div className={"bio-card__container"}>
-        <div>
-          <h1 className="bio-card__bio-name">
-            {props.currentBio.first_name} {props.currentBio.last_name}
-          </h1>
-          <div className="bio-card__info">
-            <h3 className="bio-card__bio-title">{props.currentBio.title}</h3>
-            {/* <div className={"bio-card__icon-links"}> */}
-            {props.currentBio.personal_site && (
-              <a
-                href={props.currentBio.personal_site}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <MdCode />
-              </a>
-            )}
-            {props.currentBio.linkedin && (
-              <a
-                href={props.currentBio.linkedin}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <BsLinkedin />
-              </a>
-            )}
-            {props.currentBio.github && (
-              <a
-                href={props.currentBio.github}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                <RiGithubFill />
-              </a>
-            )}
-            {/* </div> */}
-          </div>
+    <div className={"bio-card__container"}>
+      <div className={"bio-card__bio-content"}>
+        <h1 className="bio-card__bio-name">
+          {props.currentBio.first_name} {props.currentBio.last_name}
+        </h1>
+        <div className="bio-card__info">
+          <h3 className="bio-card__bio-title">{props.currentBio.title}</h3>
+          {/* <div className={"bio-card__icon-links"}> */}
+          {props.currentBio.personal_site && (
+            <a
+              href={props.currentBio.personal_site}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <MdCode />
+            </a>
+          )}
+          {props.currentBio.linkedin && (
+            <a
+              href={props.currentBio.linkedin}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <BsLinkedin />
+            </a>
+          )}
+          {props.currentBio.github && (
+            <a
+              href={props.currentBio.github}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <RiGithubFill />
+            </a>
+          )}
+          {/* </div> */}
         </div>
-        <div className={"bio-card__bio-content"}>
-          <div className={"bio-card__text-frame"}>
-            <div
-              className={"bio-card__text"}
-              dangerouslySetInnerHTML={{ __html: props.currentBio.bio_text }}
-            ></div>
-          </div>
-          <div className={"bio-card__headshot-frame"}>
-            <img
-              src={props.currentBio.headshot_url}
-              alt={props.currentBio.headshot_alt}
-              className="bio-card__headshot-image"
-            />
-          </div>
+        <div className={"bio-card__text-frame"}>
+          <div
+            className={"bio-card__text"}
+            dangerouslySetInnerHTML={{ __html: props.currentBio.bio_text }}
+          ></div>
         </div>
       </div>
-    </>
+      <div className={"bio-card__headshot-frame"}>
+        <img
+          src={props.currentBio.headshot_url}
+          alt={props.currentBio.headshot_alt}
+          className="bio-card__headshot-image"
+        />
+      </div>
+      <Fire />
+    </div>
   );
 }
