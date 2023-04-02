7b03515e3fb895e6a4ead1ffba717fbc1f6f111d
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2023-04-02 13:44:22 -0500

    Breakup contact panel into dedicated page

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Footer from "../../Components/design/Footer/Footer";
 import NavbarSplashpage from "../../Components/design/Navbar/NavbarSplashpage/NavbarSplashpage";
+import SplashpageContactForm from "../../Components/Layouts/SplashpageLayout/SplashpageContact/SplashpageContactForm";
 import "./ContactPage.css";
 
 export default function ContactPage() {
   return (
-    <div className="team-page">
+    <div className="contact-page">
       <NavbarSplashpage />
-      <main className="team-page__content">
-        <section className="team-page__text">
-          <h1 className="team-page__header">Our Team</h1>
-          <p>Contact PAGE</p>
+      <main className="contact-page__content">
+        <section>
+          <h1 className="contact-page__header">Contact Us</h1>
+          <p className="contact-page__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu
+            odio ut sem nulla pharetra diam sit amet.
+          </p>
         </section>
+        <SplashpageContactForm />
       </main>
       <Footer />
     </div>
