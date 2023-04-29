281c344b58639fe0a208608f39494ee47867c7cb
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2023-04-29 13:13:40 -0500

    Use correct case for file path
    This was failing in CI which runs on linux but not locally on macos
    (which is case-insensitive).

--- src/pages/TeamMember/TeamMemberPage.jsx
+++ src/pages/TeamMember/TeamMemberPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import BioCard from "../../Components/Layouts/SplashpageLayout/SplashpageTeam/BioCard/BioCard";
-import { TEAM_BIOS } from "../../config/TeamBios";
+import { TEAM_BIOS } from "../../config/teamBios";
 import NavbarSplashpage from "../../Components/design/Navbar/NavbarSplashpage/NavbarSplashpage";
 import Footer from "../../Components/design/Footer/Footer";
 import "./TeamMemberPage.css";
