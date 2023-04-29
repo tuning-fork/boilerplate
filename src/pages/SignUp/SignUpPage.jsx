bc02cfd39494c1afb1e13712b140f8e90ed84ad2
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2023-04-29 13:27:41 -0500

    Use react query in signup form

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useMutation } from "react-query";
 import { useHistory, Link } from "react-router-dom";
 import Footer from "../../Components/design/Footer/Footer";
 import NavbarSplashpage from "../../Components/design/Navbar/NavbarSplashpage/NavbarSplashpage";
@@ -11,18 +12,23 @@ export default function SignUpPage() {
   const history = useHistory();
   const { login } = useCurrentUser();
 
-  // TODO: maybe use react-query here
-  const handleSubmit = (newUserFields) => {
-    createUser(newUserFields)
-      .then(() => login(newUserFields.email, newUserFields.password))
-      .then(() => history.push(`/organizations`))
-      .catch((error) => {
+  const { mutate: createAndLoginUser } = useMutation(
+    async (newUserFields) => {
+      await createUser(newUserFields);
+      await login(newUserFields.email, newUserFields.password);
+    },
+    {
+      onSuccess() {
+        history.push(`/organizations`);
+      },
+      onError(error) {
         alert(
-          error.response.data.errors[0] ||
+          error.response?.data?.errors?.[0] ||
             "Something went wrong while creating this user. Please try again later."
         );
-      });
-  };
+      },
+    }
+  );
 
   return (
     <div className="sign-up-page">
@@ -36,7 +42,7 @@ export default function SignUpPage() {
             odio ut sem nulla pharetra diam sit amet.
           </p>
         </section>
-        <SignUpForm onSubmit={handleSubmit} />
+        <SignUpForm onSubmit={createAndLoginUser} />
         <p className="sign-up__login">
           Already have an account? <Link to="/login">Log In</Link>
         </p>
