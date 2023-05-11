bacafaa24b714a5c660bf126bb7dc787e3dfcb3c
Michael-McFaddin <52680887+Michael-McFaddin@users.noreply.github.com>
2023-05-10 21:04:14 -0400

    feat: additional drag and drop tests

--- cypress/e2e/grantShowOverview.cy.js
+++ cypress/e2e/grantShowOverview.cy.js
@@ -29,15 +29,84 @@ describe("Grant overview DnD", () => {
           );
         });
     });
+
     // Drag and drop one section and check that Save and Undo are enabled
     cy.get('[data-testid="Section #1"]').move({
-      deltaY: 600,
+      deltaY: 500,
       force: true,
     });
+    cy.wait(2000);
+    cy.get("button:contains('Save')").should(
+      "not.have.class",
+      "button--disabled"
+    );
+    cy.get("button:contains('Undo')").should(
+      "not.have.class",
+      "button--disabled"
+    );
+    cy.get("button:contains('Redo')").should("have.class", "button--disabled");
 
     // Save and check that all buttons are disabled
+    cy.get("button:contains('Save')").click();
+    cy.get("button:contains('Save')").should("have.class", "button--disabled");
+    cy.reload();
+    cy.get("button:contains('Undo')").should("have.class", "button--disabled");
+    cy.get("button:contains('Redo')").should("have.class", "button--disabled");
 
-    // Drag another section, hit Undo and check that Save and Undo are disabled and Redo is enabled
+    // Drag another section, select Undo and check that Save and Undo are disabled and Redo is enabled
+    cy.get('[data-testid="Section #2"]').move({
+      deltaY: 400,
+      force: true,
+    });
+    cy.wait(2000);
+    cy.get("button:contains('Undo')").click();
+    cy.get("button:contains('Save')").should("have.class", "button--disabled");
+    cy.get("button:contains('Undo')").should("have.class", "button--disabled");
+    cy.get("button:contains('Redo')").should(
+      "not.have.class",
+      "button--disabled"
+    );
+
+    // Drag a section, select Rndo and check that Save and Uedo is active
+    cy.get('[data-testid="Section #3"]').move({
+      deltaY: 300,
+      force: true,
+    });
+    cy.wait(2000);
+    cy.get("button:contains('Redo')").click();
+    cy.get("button:contains('Save')").should(
+      "not.have.class",
+      "button--disabled"
+    );
+    cy.get("button:contains('Undo')").should(
+      "not.have.class",
+      "button--disabled"
+    );
+    cy.get("button:contains('Redo')").should("have.class", "button--disabled");
+
+    /* Drag remaining sections so that the original order is 
+    reversed */
+    cy.get('[data-testid="Section #2"]').move({
+      deltaY: 400,
+      force: true,
+    });
+    cy.wait(2000);
+    cy.get('[data-testid="Section #4"]').move({
+      deltaY: 200,
+      force: true,
+    });
+    cy.wait(2000);
+    cy.get('[data-testid="Section #5"]').move({
+      deltaY: 200,
+      force: true,
+    });
+    cy.wait(2000);
+
+    // Save
+
+    // Grab the sections and check that the order is reversed
+
+    // Select redo and after each selection check that the order is as expected
 
     // Undo one section
     // Drag and drop one section
